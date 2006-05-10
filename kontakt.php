0a7313cb6baad9350be0c466a6c7e8f263ddd317
Holger Lindemann <hli@lx-system.de>
2006-05-10 07:09:48 +0000

    Funktion chkTabel verschoben

--- kontakt.php
+++ kontakt.php
@@ -1,9 +1,8 @@
 <?
-// $Id: kontakt.php,v 1.3 2005/11/02 10:37:51 hli Exp $
+// $Id$
 	require_once("inc/stdLib.php");
  	include("inc/template.inc");
 	include("inc/persLib.php");
-	include("inc/UserLib.php");
 	$co=getKontaktStamm($_GET["id"]);
 	if ($co["cp_cv_id"]) {
 		$Table=chkTable($co["cp_cv_id"]);

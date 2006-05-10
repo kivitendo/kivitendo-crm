27989bd77ab7e4fcb22a3521b1b27c40c4fd6e71
Holger Lindemann <hli@lx-system.de>
2006-05-10 07:55:50 +0000

    Logging verändert

--- inc/db.php
+++ inc/db.php
@@ -1,13 +1,15 @@
 <?
 class myDB extends DB {
 
- var $rc = false;
- var $showErr = false;
  var $db = false;
- var $debug = false;
- var $log = true;
+ var $rc = false;
+ var $showErr = false; // Browserausgabe
+ var $debug = false; // 1 = SQL-Ausgabe, 2 = zusätzlich Ergebnis
+ var $log = false;  // Alle Abfragen mitloggen
  var $errfile = "tmp/lxcrm.err";
+ var $logfile = "tmp/lxcrm.log";
  var $efh = false;
+ var $lfh = false;
  var $stime = false;
  var $ltime = 1000;
  
@@ -15,6 +17,13 @@ class myDB extends DB {
  		return ((time - $this->stime)<1000)?true:false;
  	}
 	function dbFehler($sql,$err) {
+		$this->efh=fopen($this->errfile,"a");
+		fputs($this->efh,date("Y-m-d H:i:s ->"));
+		fputs($this->efh,$sql."\n");
+		fputs($this->efh,$err."\n");
+		fputs($this->efh,print_r($this->rc,true));
+		fputs($this->efh,"\n");
+		fclose($this->efh);
 		if ($this->showErr)
 			echo "</td></tr></table><font color='red'>$sql : $err</font><br>";
 	}
@@ -27,27 +36,30 @@ class myDB extends DB {
 		};
 	}
 
-	function writeErr($txt) {
-		if (!$this->efh) $this->efh=fopen($this->errfile,"a");
-		fputs($this->efh,date("Y-m-d H:i:s ->"));
-		fputs($this->efh,$txt."\n");
-		fputs($this->efh,print_r($this->rc,true));
-		fputs($this->efh,"\n");
+	function writeLog($txt) {
+		if (!$this->lfh) $this->lfh=fopen($this->logfile,"a");
+		fputs($this->lfh,date("Y-m-d H:i:s ->"));
+		fputs($this->lfh,$txt."\n");
+		fputs($this->lfh,print_r($this->rc,true));
+		fputs($this->lfh,"\n");
 	}
 
 	function closeErr() {
 		fclose($this->efh);
 	}
+	function closeLog() {
+		fclose($this->lfh);
+	}
 	
 	function myDB($dns) {
 		$dns="pgsql://".$dns;
 		$this->db=DB::connect($dns);
-		if (!$this->db) DB::dbFehler("",$this->db->getDebugInfo());
-		if (DB::isError($this->db)) {
-			$this->writeErr("Connect",$this->db->getDebugInfo());
-			$this->dbFehler("Connect",$this->db->getDebugInfo());
-			die ($this->db->getDebugInfo());
+		if (!$this->db || DB::isError($this->db)) {
+			if ($this->log) $this->writeLog("Connect $dns");
+			$this->dbFehler("Connect $dns",$this->db->getMessage()); 
+			die ($this->db->getMessage());
 		}
+		if ($this->log) $this->writeLog("Connect ".$this->db->getDebugInfo());
 		$this->stime=time();
 		return $this->db;
 	}
@@ -56,7 +68,7 @@ class myDB extends DB {
 		$this->stime=time();
 		$this->rc=@$this->db->query($sql);
 		if ($this->debug) $this->showDebug($sql);
-		if ($this->log) $this->writeErr($sql);
+		if ($this->log) $this->writeLog($sql);
 		if(DB::isError($this->rc)) {
 			$this->dbFehler($sql,$this->rc->getMessage());
 			$this->rollback();
@@ -80,7 +92,7 @@ class myDB extends DB {
 		$this->stime=time();
 		$this->rc=$this->db->getAll($sql,DB_FETCHMODE_ASSOC);
 		if ($this->debug) $this->showDebug($sql);
-		if ($this->log) $this->writeErr($sql);
+		if ($this->log) $this->writeLog($sql);
 		if(DB::isError($this->rc)) {
 			$this->dbFehler($sql,$this->rc->getMessage());
 			return false;

551dd573a528610085707acc4765a5e494187788
Holger Lindemann <hli@lx-system.de>
2006-10-06 09:35:18 +0000

    Ablösung der menu1.php, verbesserte Informationen

--- showsess.php
+++ showsess.php
@@ -1,16 +1,19 @@
 <?
 session_start();
-if ($_POST["ok"]) {
+if ($_GET["ok"]) {
+	echo "<pre>";
+	print_r($_SESSION);
+	echo "</pre>";
+	echo "<form name='x' action='showsess.php' method='post'>";
+	echo "<input type='submit' name='del' value='Session l&ouml;schen'>";
+	echo "</form>";
+} else {
 	while( list($key,$val) = each($_SESSION) ) {
 		unset($_SESSION[$key]);
 	}
+	echo "ok. Session-Variablen gel&ouml;scht.<br>";
+	echo "Rufen Sie nun einen anderen CRM-Men&uuml;punkt auf, um eine neue Session zu erzeugen";
 }
 
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
 ?>
-<form name="x" action="showsess.php" method="post">
-<input type="submit" name="ok" value="Session löschen">
-</form>
 
---
+++ status.php
@@ -0,0 +1,53 @@
+<? 
+	session_start();
+	$ver=file("VERSION");
+	$ver=$ver[0];
+	if ($_GET["test"]=="ja") {
+		require("inc/stdLib.php");
+		$rc=$db->getAll("select * from crm order by version","Status");
+		if ($rc) {
+			foreach ($rc as $row) {
+				echo $row["version"]." ".$row["datum"]." ".$row["uid"]."<br>";
+			}
+		}
+		
+	}
+?>
+<html>
+	<head><title></title>
+	<link type="text/css" REL="stylesheet" HREF="css/main.css"></link>
+<body>
+<p class="listtop">Status</p>
+
+<!--table class="karte"><tr><td class="karte"-->
+<!---------------------------------------------------------------------->
+<center>
+<?
+$db=false;
+$prog=false;
+$d = dir("tmp/");
+while (false !== ($entry = $d->read())) {
+	if (ereg("upd.*log",$entry)) echo "<a href='tmp/$entry'>$entry</a><br>\n";
+	if (ereg("instprog.log",$entry)) $prog=true;
+	if (ereg("install.log",$entry)) $db=true;
+}
+$d->close();
+if ($prog) { echo "<a href='tmp/instprog.log'>Programminstallation</a><br>"; } else { echo "Kein Logfile f&uuml;r Programminstallation<br>"; }
+if ($db) { echo "<a href='tmp/install.log'>Datenbankinstallation</a><br>"; } else { echo "Kein Logfile f&uuml;r Datenbankinstallation<br>"; }
+?>
+<table>
+	<tr><td>ProgrammVersion</td><td>[<?= $ver ?>]</td></tr>
+	<tr><td>Datenbank:</td><td> [<?= $_SESSION["dbname"] ?>]</td></tr>
+	<tr><td>db-Server:</td><td>[<?= $_SESSION["dbhost"] ?>]</td></tr>
+	<tr><td>Benutzer:</td><td>[<?= $_SESSION["employee"] ?>:<?= $_SESSION["loginCRM"] ?>]</td></tr>
+	<tr><td>Session-ID:</td><td>[<?= session_id() ?>]</td></tr>
+	<tr><td>PHP-Umgebung:</td><td>[<a href="info.php">anzeigen</a>]</td></tr>
+	<tr><td>Session<a href="showsess.php?ok=show">:</a></td><td>[<a href="showsess.php">l&ouml;schen</a>]</td></tr>
+	<tr><td>db-Zugriff:</td><td>[<a href="status.php?test=ja">testen</a>]</td></tr>
+</table>
+<a href="showsess.php">
+</center>
+<!---------------------------------------------------------------------->
+<!--/td></tr></table-->
+</body>
+</html>

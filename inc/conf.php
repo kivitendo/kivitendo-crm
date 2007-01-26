afeea9cc577035ebd5f96fc0b09a0d7c85c1e1d0
Holger Lindemann <hli@lx-system.de>
2007-01-26 20:38:08 +0000

    Routernplaner

--- inc/conf.php
+++ inc/conf.php
@@ -18,11 +18,23 @@ $cp_sonder=array(1 => "News", 2 => "Test 1");
 $logmail=true;
 $jcalendar=true;
 $listLimit=200;
+$tinymce=false;
 $jpg=false;
 $showErr=false;
 $CallEdit = true;
 $CallDel = true;
-$planspace="-";
-$stadtplan="http://www.goyellow.de/map/%TOZIPCODE%-%TOCITY%/%TOSTREET%";
-$ERPNAME="unstable"
+//Verschiedene Map-Anbieter können hier eingestellt werden
+//Leerzeichenersatz
+//GoYellow
+//$planspace="-";
+//$stadtplan="http://www.goyellow.de/map/%TOZIPCODE%-%TOCITY%/%TOSTREET%";
+//URL
+//viamichelin
+//$planspace="+";
+//$stadtplan="http://www.viamichelin.de/viamichelin/deu/dyn/controller/mapPerformPage?strAddress=%TOSTREET%&strCP=%TOZIPCODE%&strLocation=%TOCITY%&strDestCP=89073&strDestLocation=Ulm&strDestAddress=Ensingerstr+11";
+//Google
+//http://maps.google.de/maps?f=d&hl=de&saddr=89073+ulm,+ulmergasse+11&daddr=Hafenstra%C3%9Fe+5,+24837+Schleswig&sll=51.399206,9.887695&sspn=11.934881,29.443359&ie=UTF8&z=5&ll=51.454007,9.887695&spn=11.920677,29.443359&om=1
+$planspace="+";
+$stadtplan="http://maps.google.de/maps?f=d&hl=de&daddr=%TOSTREET%,%TOZIPCODE%+%TOCITY%&saddr=%FROMSTREET%,%FROMZIPCODE%+%FROMCITY%";
+$ERPNAME="unstable";
 ?>

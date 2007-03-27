b0bec9c6eef4e378730ee60507a758396f2428a0
Holger Lindemann <hli@lx-system.de>
2007-03-27 13:30:34 +0000

    Mailvorlage, Briefanrede,extrafelder

--- update/update131-140.sql
+++ update/update131-140.sql
@@ -1,9 +1,3 @@
-CREATE TABLE mailvorlage (
-        id integer DEFAULT nextval('crmid'::text) NOT NULL,
-        cause char varying(120),
-        c_long text,
-        employee integer
-);
 alter table contacts add column cp_salutation text;
 alter table documents add column pfad text;
 ALTER TABLE vendor ADD COLUMN lead integer;

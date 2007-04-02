dd8aa71fefd359f5d6f2de1e53201b0b3ae8d2f9
Jochen Dierolf <jdierolf@lx-systems.de>
2007-04-02 17:51:02 +0000

    Einige grundlegende Umbaumaßnahmen. Neue Klassen hinzugefügt.

--- services/header.inc.php
+++ services/header.inc.php
@@ -1,46 +1,61 @@
 <?php
-	/**************************************************************************\	
-	* This file was originaly written by Jan Dierolf (jadi75@gmx.de)           *
-	* ------------------------------------------------------------------------ *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
-	\**************************************************************************/
+    /**************************************************************************\
+    * This file was originaly written by Jan Dierolf (jadi75@gmx.de)           *
+    * ------------------------------------------------------------------------ *
+    *  This program is free software; you can redistribute it and/or modify it *
+    *  under the terms of the GNU General Public License as published by the   *
+    *  Free Software Foundation; either version 2 of the License, or (at your  *
+    *  option) any later version.                                              *
+    \**************************************************************************/
 
+    $inclpath=ini_get('include_path');
+    ini_set('include_path',$inclpath.":./services:../");
 
-	// Insert your lx paths (erp and crm)
-	define('LX_ERP_ROOT','/var/www/unstable/');
-	define('LX_CRM_ROOT',LX_ERP_ROOT. 'crm/'); // not in use at the moment
-	
-	// Should not be changed
-	define('LX_CRM_SERVICE_ROOT', LX_CRM_ROOT. 'services/');	
-	
-	
 	session_start();
-	
 
-//	require_once (LX_CRM_SERVICE_ROOT . 'inc/class.database.inc.php');
-	require_once (LX_CRM_ROOT . 'inc/db.php');
-	require_once (LX_CRM_SERVICE_ROOT . 'inc/global_functions.inc.php');
-	
-	
+	require ('../inc/conf.php');
+    require ('inc/globalfunctions.inc.php');
+    require ('inc/class.logger.inc.php');
 
-	$GLOBALS['lxlogin'] = '';									
-	$GLOBALS['lxpasswd'] = '';
-	$GLOBALS['lxemployee_id'] = '';	
-   	$GLOBALS['lxlogin'] = '';	
-	$GLOBALS['lxdb'] = null;	
+	define('M_SERVICES', 'services');	
 	
-	
-	function login($user, $passwd) {
-			
-		ini_set("gc_maxlifetime","3600");
-		
-		$userfile = LX_ERP_ROOT."users/".$user.".conf";
-		
-		if(!is_readable($userfile))
-			return false;
+	//--- some initialization stuff
+	// initialize logging service
+    Logger::setLevel(L_DEBUG3);
+    Logger::setLogPath("./");
+    Logger::initModule(M_SERVICES, "a");
+    
+    Logger::log(M_SERVICES, L_DEBUG3, "Session:\n".print_array($_SESSION),__FILE__,__LINE__);   
+    
+    $headers = getallheaders();
+    
+    //---
+
+	// helper
+    function print_array($array) {
+//    	$ret = '';
+//	    if(is_array($array)) foreach ($array as $key => $val) {
+//	    	if(is_string($val))
+//    			$ret .= "$key => $val\n";
+//	    	else if(is_array($val) || is_object($val)) {	    	
+//	    		$ret .= "$key => ".print_array($val, &$ret)."\n";
+//	    	}    	
+//	    }	
+	    return print_r($array, TRUE);
+    }
+    
+    // do login stuff
+    function login($user, $passwd) {
+        global $ERPNAME, $db;
+        ini_set("gc_maxlifetime","3600");
+        
+        Logger::log(M_SERVICES, L_INFO, "User ${user}(${passwd}) try to login",__FILE__,__LINE__);
+
+        $userfile = "../../".$ERPNAME."/users/".$user.".conf";
+
+        if(!is_readable($userfile))
+            return false;
+            
 		$tmp = @file_get_contents($userfile);
 		preg_match("/dbname => '(.+)'/",$tmp,$hits);
 		$dbname=$hits[1];
@@ -52,85 +67,101 @@
 		$dbhost=$hits[1];
 		preg_match("/dbport => '(.+)'/",$tmp,$hits);
 		$dbport=$hits[1];
-		preg_match("/password => '(.+)'/",$tmp,$hits);
-		$pwd=$hits[1];
-		$rr = crypt($passwd, substr($user,0,2));
-		if($pwd != $passwd && $pwd != crypt($passwd, substr($user,0,2)))
-			return false;
-		
-		$GLOBALS['lxlogin'] = $user;									
-		$GLOBALS['lxpasswd'] = $pwd;
-		
 		if (!$dbhost) $dbhost="localhost";
-//		chkdir($dbname);
-	   	$GLOBALS['lxlogin']=$user;	
-		$GLOBALS['lxdb']= CreateObject('Database', $dbhost, $dbuser, $dbpasswd,$dbname,$dbport);	
 
+
+        preg_match("/password => '(.+)'/",$tmp,$hits);
+        $pwd=$hits[1];
+        if($pwd != $passwd && $pwd != crypt($passwd, substr($user,0,2)))
+            return false;                    
+
+	   	$_SESSION["employee"]= $user;
+	   	$_SESSION["password"]= $pwd;
+		$_SESSION["mansel"]=$dbname;
+		$_SESSION["dbname"]=$dbname;
+		$_SESSION["dbhost"]=$dbhost;
+		$_SESSION["dbport"]=(empty($dbport))?5432:$dbport;
+		$_SESSION["dbuser"]=$dbuser;
+		$_SESSION["dbpasswd"]=$dbpasswd;		
+		Logger::log(M_SERVICES, L_INFO, "Open db connection: ".$_SESSION["dbhost"]."@".$_SESSION["dbpasswd"].":".$_SESSION["dbuser"]."/".$_SESSION["dbname"].":".$_SESSION["dbport"],__FILE__,__LINE__);
+		$_SESSION["db"]= &CreateObject("Database",$_SESSION["dbhost"],$_SESSION["dbuser"],$_SESSION["dbpasswd"],$_SESSION["dbname"],$_SESSION["dbport"],false);		
+    	$db = $_SESSION["db"];
+    	
 		$sql="select * from employee where login='$user'";
-		$rs=$GLOBALS['lxdb']->getAll($sql);
+		$rs=$_SESSION["db"]->getAll($sql);
 		if(!$rs) {
 			return false;
-		} 
-		else {
-			$tmp=$rs[0];
-			$GLOBALS['lxemployee_id'] = $tmp['id'];
-			$_SESSION['login'] = $user;
-			$_SESSION['password'] = $pwd;
-			return true;
-		}
-		
-	}
-	
-	
-	/* Note: this command only available natively in Apache (Netscape/iPlanet/SunONE in php >= 4.3.3) */
-	if(!function_exists('getallheaders'))
-	{
-		function getallheaders()
-		{
-			settype($headers,'array');
-			foreach($_SERVER as $h => $v)
-			{
-				if(ereg('HTTP_(.+)',$h,$hp))
-				{
-					$headers[$hp[1]] = $v;
-				}
+		} else {
+			if ($rs) {
+				$tmp=$rs[0];
+				$_SESSION["termbegin"]=(($tmp["termbegin"]>=0)?$tmp["termbegin"]:8);
+				$_SESSION["termend"]=($tmp["termend"])?$tmp["termend"]:19;
+				$_SESSION["Pre"]=$tmp["pre"];
+				$_SESSION["interv"]=($tmp["interv"]>0)?$tmp["interv"]:60;
+				$_SESSION["loginCRM"]=$tmp["id"];
+				$_SESSION["lang"]=$tmp["countrycode"]; //"de";
+				$_SESSION["kdview"]=$tmp["kdview"];
+				$sql="select * from defaults";
+				$rs=$_SESSION["db"]->getAll($sql);
+				$_SESSION["ERPver"]=$rs[0]["version"];
+				
+				return true;
+			} else {
+				return false;
 			}
-			return $headers;
 		}
-	}
-	
-	function verifyuser() {
-	
-		$headers = getallheaders();
-	
-//	    $fp = fopen('xml_rpc_out.log','a+');	
-//		fwrite($fp,"==== GOT ============================\n" . $GLOBALS['HTTP_RAW_POST_DATA']
-//			. "\n==== RETURNED =======================\n");	
-//		fclose($fp);
-		
-		$isodate = isset($headers['isoDate']) ? $headers['isoDate'] : $headers['isodate'];
-		$isodate = ($isodate == 'simple') ? True : False;
-	//	$server->setSimpleDate($isodate);
-		$auth_header = isset($headers['Authorization']) ? $headers['Authorization'] : $headers['authorization'];
-	
-		$auth = false;
-		if(eregi('Basic *([^ ]*)',$auth_header,$auth))
-		{
-			list($login, $passwd) = explode(':',base64_decode($auth[1]));
-			if(login($login, $passwd))
-				$auth = true;		
-			else			
-				$auth = false;	
-			
-		}	
-		
-	//	echo 'Login: '. $_SESSION['login']. ', password: '. $_SESSION['password'];
-		if(!$auth) {
-			
-			$auth = login($_SESSION['login'], $_SESSION['password']);
-		}
-		
-		return $auth;
-	}
-	
+
+    }
+
+
+    /* Note: this command only available natively in Apache (Netscape/iPlanet/SunONE in php >= 4.3.3) */
+    if(!function_exists('getallheaders'))
+    {
+        function getallheaders()
+        {        	
+            settype($headers,'array');
+            foreach($_SERVER as $h => $v)
+            {
+                if(ereg('HTTP_(.+)',$h,$hp))
+                {
+                    $headers[$hp[1]] = $v;
+                }
+            }
+            return $headers;
+        }
+    }
+    
+    // check user verfication
+    function verifyuser() {
+		global $headers;
+//      $fp = fopen('xml_rpc_out.log','a+');
+//      fwrite($fp,"==== GOT ============================\n" . $GLOBALS['HTTP_RAW_POST_DATA']
+//          . "\n==== RETURNED =======================\n");
+//      fclose($fp);
+		Logger::log(M_SERVICES, L_DEBUG3, "==== GOT ============================\n" . $GLOBALS['HTTP_RAW_POST_DATA']
+								          . "\n==== RETURNED =======================\n",__FILE__,__LINE__);
+
+
+        $auth_header = isset($headers['Authorization']) ? $headers['Authorization'] : $headers['authorization'];
+
+		Logger::log(M_SERVICES, L_DEBUG3, "HEADERS: \n".print_array($headers),__FILE__,__LINE__);
+        $auth = false;
+        if(eregi('Basic *([^ ]*)',$auth_header,$auth))
+        {
+            list($login, $passwd) = explode(':',base64_decode($auth[1]));
+            if(login($login, $passwd))
+                $auth = true;
+            else
+                $auth = false;
+
+        }
+
+        if(!$auth) {
+
+            $auth = login($_SESSION['employee'], $_SESSION['password']);
+        }
+
+        return $auth;
+    }
+
 ?>
--- services/xmlrpc.php
+++ services/xmlrpc.php
@@ -1,5 +1,5 @@
 <?php
-	/**************************************************************************\	
+	/**************************************************************************\
 	* This file was originaly written by Jan Dierolf (jadi75@gmx.de)           *
 	* ------------------------------------------------------------------------ *
 	*  This program is free software; you can redistribute it and/or modify it *
@@ -8,21 +8,41 @@
 	*  option) any later version.                                              *
 	\**************************************************************************/
 
-	require_once('header.inc.php');
-	
-	require_once(LX_CRM_SERVICE_ROOT.'inc/class.xmlrpcserver.inc.php');
-	require_once(LX_CRM_SERVICE_ROOT.'inc/class.xmlrpccontacts.inc.php');
-	
+	require_once('./header.inc.php');
 
-	XMLRPCServer::instance()->authed = verifyuser();
+	require_once('inc/class.xmlrpcserver.inc.php');
+	require_once('inc/class.xpcegwcontacts.inc.php');
 	
-//	echo 'Login: '. $_SESSION['login']. ', password: '. $_SESSION['password']. 'auth='. (XMLRPCServer::instance()->authed ? 'true' : 'false');
+    
+
+	$server = XMLRPCServer::instance();
+	// check date format
+	$isodate = isset($headers['isoDate']) ? $headers['isoDate'] : $headers['isodate'];
+    $isodate = ($isodate == 'simple') ? True : False;
+	// set date format
+	$server->setSimpleDate($isodate);
 	
-	$xmlrpccontacts = new XMLRPCContacts($GLOBALS['lxdb']);
-			
-	XMLRPCServer::instance()->addService($xmlrpccontacts->get_rpc_list());
-	
-   	XMLRPCServer::instance()->doService();	
-			
+	// verify incoming user
+	$server->authed = verifyuser();
+	if(!$server->authed) {
+		Logger::log(M_SERVICES, L_DEBUG3, "Request without, userdata -> allow only system services",__FILE__,__LINE__);
+		$server->doSystemService();
+    	Logger::log(M_SERVICES, L_INFO, "*** service finished ***",__FILE__,__LINE__);
+		return;		
+	}
+
+    Logger::log(M_SERVICES, L_DEBUG3, 'Login: '. $_SESSION['employee']. ', password: '. $_SESSION['password']. ', auth='. ($server->authed ? 'true' : 'false'),__FILE__,__LINE__);
+    
+
+	// create contact service handler based on kaddressbook <=> egroupware interface
+	$xmlrpccontacts = new XPCEGWContacts($_SESSION['db'], $_SESSION['loginCRM']);
+
+	// add service to the server
+	$server->addService($xmlrpccontacts->get_rpc_list());
+	// handle service request
+   	$server->doService();
+    Logger::log(M_SERVICES, L_INFO, "*** service finished ***",__FILE__,__LINE__);
+
+
 
 ?>

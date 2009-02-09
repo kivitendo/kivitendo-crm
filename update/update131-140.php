832cc8195a6ed152c9599fea7181423fe2acefcd
Holger Lindemann <hli@lx-system.de>
2009-02-09 11:21:44 +0000

    dev. Fehler, Universalupdate von 120, 130, 131

--- update/update131-140.php
+++ update/update131-140.php
@@ -1,60 +1,38 @@
-<?
-if (!is_writable("tmp/upd131140.log")) {
-        $log=fopen("tmp/upd131140.log","a");
-} else {
-        $log=fopen("/tmp/upd131140.log","a");
-        echo "Logfile in /tmp<br> Schreibrechte im CRM-Verzeichnis pr&uuml;fen<br>";
+<?php
+if ($_POST) {
+	session_start();
+	chdir("..");
+	include("inc/conf.php");
+	include("inc/db.php");
+	include("inc/loginok.php");
+	$updatefile="update".$_POST["oldver"]."-$VERSION";
+	$updatefile=str_replace(".","",$updatefile);
+	if (! $log=@fopen("tmp/".$updatefile.".log","a") ) {
+		if ($log=@fopen("/tmp/".$updatefile.".log","a") ) {
+			echo "Logfile in /tmp<br> Schreibrechte im CRM-Verzeichnis pr&uuml;fen<br>";
+		} else {
+			echo "Kann kein Logfile $updatefile.log (2) erstellen<br>";
+			exit(1);
+		}
+	}
 }
 fputs($log,date("d.m.Y H:i:s")."\n");
-fputs($log,"131->140\n");
-echo "Update auf Version $VERSION<br>";
-echo "Vorraussetzungen pr&uuml;fen:<br>";
-        $path=ini_get("include_path");
-        fputs($log,"Suchpfad: $path\n");
-        $pfade=split(":",$path);
-        $chk=array ("xajax/xajax.inc");
-        $ok=true;
-        foreach($chk as $file) {
-                echo "$file: ";
-                $ook=false;
-                fputs($log,"$file: ");
-                foreach($pfade as $path) {
-                        if (is_readable($path."/".$file.".php")) {
-                                $ook=true;
-                                break;
-                        }
-                }
-                if ($ook) {
-                        echo "ok<br>";
-                        fputs($log,"ok\n");
-                } else {
-                        $ok=false;
-                        echo "false<br>";
-                        fputs($log,"false\n");
-                }
-        }
-?>
-Die Verzeichnisstruktur der Dokumente (Kunden/Lieferanten/Kontakte) hat sich ge&auml;ndert.<br>
-Das Script versucht die Verzeichnisse entsprechent umzubenennen und zu verschieben.<br>
-Bitte erstellen Sie zun&auml;chst ein Backup des Dokumentenverzeichnis (crm/dokumente/).
-<form name="update" method="post" action="">
-	<input type="submit" name="ok" value="ok">
-	<input type="submit" name="nok" value="nee - lieber nicht">
-</form>
-<?
+fputs($log,$_POST["oldver"]."->140\n");
+
 if ($_POST["ok"]) {
+	fputs($log,"Update ok\n");
 	echo "Datenbankinstanz ".$_SESSION["dbname"]." erweitern : ";
-	$updatefile="update/update131-140.sql";
 	if (ob_get_level() == 0) ob_start();
 	ob_flush();
 	flush();
-	$f=fopen($updatefile,"r");
+	$f=fopen("update/".$updatefile.".sql","r");
 	if (!$f) { 
-		fputs($log,"$updatefile nicht gefunden\n");
-		echo "<br>Kann Datei $updatefile nicht &ouml;ffnen.<br>Abbruch!";
+		fputs($log,"$updatefile.sql nicht gefunden\n");
+		echo "<br>Kann Datei $updatefile.sql nicht &ouml;ffnen.<br>Abbruch!";
 		fclose($log);
 		exit();
 	}
+	echo $updatefile.".sql verwendet<br>";
 	$zeile=trim(fgets($f,1000));
 	$query="";
 	$ok=0;
@@ -75,6 +53,10 @@ if ($_POST["ok"]) {
 			$rc=$db->query(substr($query,0,-1));
 			if ($rc) { $ok++; echo "."; fputs($log,"."); }
 			else { $fehl++; echo "!"; fputs($log,"!");};
+			if ($pos % 10 == 0) {
+				echo " ";
+				fputs($log," ");
+			}
 			ob_flush();
 			flush();
 			$query="";
@@ -90,11 +72,56 @@ if ($_POST["ok"]) {
 		fputs($log,"Datenbankupdate erfolgreich\n");
 		echo "Datenbankupdate erfolgreich durchgef&uuml;hrt.<br>"; 
 	}
-	require ("newdocdir.php");
+	require ("update/newdocdir.php");
 	fclose($f);
 
 } else if ($_POST["nok"]) {
 	echo "Abbruch";
 	exit;
-}
+} else {
+echo "Vorraussetzungen pr&uuml;fen:<br>";
+        $path=ini_get("include_path");
+        fputs($log,"Suchpfad: $path\n");
+        $pfade=split(":",$path);
+        $chkfile=array("DB","DB/pgsql","fpdf","fpdi","Mail","Mail/mime","Image/Canvas","Image/Graph",
+                        "jpgraph","Contact_Vcard_Build","Contact_Vcard_Parse","xajax/xajax.inc");
+        $chkstat=array(1,1,0,0,0,0,0,0,0,0,0,1);
+        $ok=true;
+        foreach($chkfile as $file) {
+                echo "$file: ";
+                $ook=false;
+                fputs($log,"$file: ");
+                foreach($pfade as $path) {
+                        if (is_readable($path."/".$file.".php")) {
+                                $ook=true;
+                                break;
+                        }
+                }
+                if ($ook) {
+                        echo "ok<br>";
+                        fputs($log,"ok\n");
+                } else {
+                        $ok=false;
+			if ($chkstat[$pos]==1) {
+                                $dbok=false;
+                                echo "<font color='red'><b>unbedingt Erforderlich!!</b></font><br>";
+                                fputs($log,"Fehler: Erforderlich\n");
+                        } else {
+                                echo "<font color='red'>dieses Paket fehlt oder kann nicht geladen werden.</font><br>";
+                                fputs($log,"Fehler\n");
+                        }
+                }
+        }
+	fclose($log);
+?>
+Die Verzeichnisstruktur der Dokumente (Kunden/Lieferanten/Kontakte) hat sich ge&auml;ndert.<br>
+Das Script versucht die Verzeichnisse entsprechent umzubenennen und zu verschieben.<br>
+Bitte erstellen Sie zun&auml;chst ein Backup des Dokumentenverzeichnis (crm/dokumente/<?= $_SESSION["dbname"] ?>).
+<form name="update" method="post" action="update/update131-140.php">
+	<input type="hidden" name="oldver" value="<?= $rc[0]["version"] ?>">
+	<input type="submit" name="ok" value="ok">
+	<input type="submit" name="nok" value="nee - lieber nicht">
+</form>
+<?php 
+} 
 ?>

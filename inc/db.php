f89ba62eab47e57fe1df6baf5dcb1fad3d106bef
Holger Lindemann <hli@lx-system.de>
2009-03-16 15:02:36 +0000

    Funktonen für Insert + Update mit prep. Statement eingefügt

--- inc/db.php
+++ inc/db.php
@@ -14,12 +14,17 @@ class myDB extends DB {
  var $db = false;
  var $rc = false;
  var $showErr = false; // Browserausgabe
- var $debug = false; // 1 = SQL-Ausgabe, 2 = zusätzlich Ergebnis
- var $log = true;  // Alle Abfragen mitloggen
+ var $log = true;     // Alle Abfragen mitloggen
  var $errfile = "tmp/lxcrm.err";
  var $logfile = "tmp/lxcrm.log";
  var $lfh = false;
- 
+
+	/**********************************************
+	* dbFehler - Fehler in ein Log-File ausgeben
+	* IN: $sql - SQL-Statement
+	* IN: $err - Fehlermeldung
+	* OUT: NONE
+	**********************************************/ 
 	function dbFehler($sql,$err) {
 		$efh=fopen($this->errfile,"a");
 		fputs($efh,date("Y-m-d H:i:s \n"));
@@ -37,15 +42,11 @@ class myDB extends DB {
 			echo "</td></tr></table><font color='red'>$sql : $err</font><br>";
 	}
 
-	function showDebug($sql) {
-		echo $sql."<br>";
-		if ($this->debug==2) {
-			echo "<pre>";
-			print_r($this->rc);
-			echo "</pre>";
-		};
-	}
-
+	/**********************************************
+	* writelog - Texte in ein Log-File ausgeben
+	* IN: $txt - SQL-Statement oder Text
+	* OUT: NONE
+	**********************************************/ 
 	function writeLog($txt) {
 		if ($this->lfh===false)
 			$this->lfh=fopen($this->logfile,"a");
@@ -66,7 +67,12 @@ class myDB extends DB {
 		fclose($this->lfh);
 	}
 	
-	function myDB($host,$user,$pwd,$db,$port,$showErr=false) {
+	/**********************************************
+	* myDB - Konstruktor
+	* IN: $host,$user,$pwd,$db,$port - Parameter der Datenbank
+	* OUT: DB-Objekt
+	**********************************************/ 
+	function myDB($host,$user,$pwd,$db,$port) {
 		$dsn = array(
                     'phptype'  => 'pgsql',
                     'username' => $user,
@@ -75,10 +81,9 @@ class myDB extends DB {
                     'database' => $db,
                     'port'     => $port
                 );
-		$this->showErr=$showErr;
 		$this->db=DB::connect($dsn);
 		if (!$this->db || DB::isError($this->db)) {
-			if ($this->log) $this->writeLog("Connect $dns");
+			if ($this->log) $this->writeLog("Connect Error: $dns");
 			$this->dbFehler("Connect ".print_r($dsn,true),$this->db->getMessage()); 
 			die ($this->db->getMessage());
 		}
@@ -86,9 +91,14 @@ class myDB extends DB {
 		return $this->db;
 	}
 
+	/**********************************************
+	* query - beliebiges SQL-Statement absetzen
+	* IN: $sql - Statement
+	* OUT: true/false
+	**********************************************/ 
 	function query($sql) {
+		if (strpos($sql,";")>0) return false;
 		$this->rc=@$this->db->query($sql);
-		if ($this->debug) $this->showDebug($sql);
 		if ($this->log) $this->writeLog($sql);
 		if(DB::isError($this->rc)) {
 			$this->dbFehler($sql,$this->rc->getMessage());
@@ -99,19 +109,88 @@ class myDB extends DB {
 		}
 	}
 
+	/**********************************************
+	* update - einen Datensatz modifizieren
+	* IN: $table - Tabelle
+	* IN: $fields - betroffene Felder
+	* IN: $values - dazugehörige Werte
+	* IN: $where - welcher Datensatz
+	* OUT: true/false
+	**********************************************/ 
+	function update($table,$fields,$values,$where) {
+		if (strpos($where,"=")<1) {
+			$this->dbFehler('Update','Where missing or wrong: '.$where);
+			$this->dbFehler('Update',print_r(debug_backtrace(),true));
+			return false;
+		}
+		if ($this->log) {
+			$this->writeLog('Update auf: '.$table);
+			$this->writeLog(print_r($fields,true));
+			$this->writeLog(print_r($values,true));
+		}
+		//SQL-Statement vorbereiten
+		$sth = $this->db->autoPrepare($table, $fields, DB_AUTOQUERY_UPDATE,$where);
+		if (PEAR::isError($sth)) {
+			$this->dbFehler($sql,$sth->getMessage());
+			$this->rollback();
+			return false;
+		}
+		//wenn ok, ausführen
+		$this->rc=@$this->db->execute($sth, $values);
+		if(DB::isError($this->rc)) {
+			$this->dbFehler($sql,$this->rc->getMessage());
+			$this->rollback();
+			return false;
+		}
+		return true;
+	}
+
+	/**********************************************
+	* insert - einen neuen Datensatz anlegen
+	* IN: $table - Tabelle
+	* IN: $fields - betroffene Felder
+	* IN: $values - dazugehörige Werte
+	* OUT: true/false
+	**********************************************/ 
+	function insert($table,$fields,$values) {
+		if ($this->log) {
+			$this->writeLog('Insert in: '.$table);
+			$this->writeLog(print_r($fields,true));
+			$this->writeLog(print_r($values,true));
+		}
+		//SQL-Statement vorbereiten
+		$sth = $this->db->autoPrepare($table, $fields, DB_AUTOQUERY_INSERT);
+		if (PEAR::isError($sth)) {
+			$this->dbFehler($sql,$sth->getMessage());
+			$this->rollback();
+			return false;
+		}
+		//wenn ok, ausführen
+		$this->rc=@$this->db->execute($sth, $values);
+		if(DB::isError($this->rc)) {
+			$this->dbFehler($sql,$this->rc->getMessage());
+			$this->rollback();
+			return false;
+		}
+		return true;
+	}
+
 	function begin() {
+		if ($this->log) $this->writeLog('BEGIN');
 		return $this->query("BEGIN");
 	}
 	function commit() {
+		if ($this->log) $this->writeLog('COMMIT');
 		return $this->query("COMMIT");
 	}
 	function rollback() {
+		if ($this->log) $this->writeLog('ROLLBACK');
 		return $this->query("ROLLBACK");
 	}
 
 	function getAll($sql) {
+		if (strpos($sql,";")>0) return false;
 		$this->rc=$this->db->getAll($sql,DB_FETCHMODE_ASSOC);
-		if ($this->debug) $this->showDebug($sql);
 		if ($this->log) $this->writeLog($sql);
 		if(DB::isError($this->rc)) {
 			$this->dbFehler($sql,$this->rc->getMessage());
@@ -130,31 +209,13 @@ class myDB extends DB {
 		}
 	}
 	function saveData($txt) {
-		if (get_magic_quotes_gpc()) { 	
+		//if (get_magic_quotes_gpc()) { 	
+		if (get_magic_quotes_runtime()) { 	
 			return $txt;
 		} else {
 			return DB::quoteSmart($string); 
 		}
 	}
 
-	/****************************************************
-	* uudecode
-	* in: string
-	* out: string
-	* dekodiert Perl-UU-kodierte Passwort-Strings
-	* http://de3.php.net/base64_decode (bug #171)
-	*****************************************************/
-	function uudecode($encode) {
-		$encode=stripslashes($encode);
-		$b64chars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-		$encode = preg_replace("/^./m","",$encode);
-		$encode = preg_replace("/\n/m","",$encode);
-		for($i=0; $i<strlen($encode); $i++) {
-			if ($encode[$i] == '') $encode[$i] = ' ';
-			$encode[$i] = $b64chars[ord($encode[$i])-32];
-		}
-		while(strlen($encode) % 4) $encode .= "=";
-		return base64_decode($encode);
-	}
 }
 ?>

96c62cf777c599aca19e36c34f44a6922893315f
Holger Lindemann <hli@lx-system.de>
2009-05-28 11:20:00 +0000

    Das aktuelle File

--- update/blz.php
+++ update/blz.php
@@ -7,26 +7,39 @@ BLZimport mit Browser nach Lx-Office ERP
 Holger Lindemann <hli@lx-system.de>
 */
 
-if ($_POST) {
 
-function ende($nr) {
-	echo "Abbruch: $nr<br>";
-	echo "Fehlende oder falsche Daten.";
-	exit(1);
-}
-
-if (!$_SESSION["db"]) {
-	$conffile="../config/authentication.pl";
-	if (!is_file($conffile)) {
-		ende(4);
+/* display help */
+if ($_POST["ok"]=="Hilfe") {
+	echo "<br>Die erste Zeile enth&auml;lt keine Feldnamen der Daten.<br>";
+	echo "Die Datenfelder haben eine feste Breite.<br><br>"; 
+	echo "Die Daten k&ouml;nnen hier bezogen werden:<br>";
+	echo "<a http='http://www.bundesbank.de/zahlungsverkehr/zahlungsverkehr_bankleitzahlen_download.php'>";
+	echo "http://www.bundesbank.de/zahlungsverkehr/zahlungsverkehr_bankleitzahlen_download.php</a>";
+	exit(0);
+} else if ($_POST) {
+	function ende($nr) {
+		echo "Abbruch: $nr<br>";
+		echo "Fehlende oder falsche Daten.";
+		exit(1);
 	}
-}
-require ("import_lib.php");
 
-if (!anmelden()) ende(5);
-/* get DB instance */
-$db=$_SESSION["db"]; //new myDB($login);
+	function l2u($str) {
+		return iconv("ISO-8859-1", "UTF-8",$str);
+	}
 
+	require ("import_lib.php");
+
+	if (!$_SESSION["db"]) {
+		$conffile="../config/authentication.pl";
+		if (!is_file($conffile)) {
+			ende(4);
+		}
+		if (!anmelden()) ende(5);
+	}
+
+
+	/* get DB instance */
+	$db=$_SESSION["db"]; 
 
 	$test=$_POST["test"];
 
@@ -51,33 +64,71 @@ $db=$_SESSION["db"]; //new myDB($login);
 
 	$sqlins="INSERT INTO blz_data (blz,fuehrend,bezeichnung,plz,ort,kurzbez,pan,bic,pzbm,nummer,aekz,bl,folgeblz) ";
 	$sqlins.="VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s',%d,'%s','%s','%s')";
-	$sqldel="delete from blz";
+	$teststr="<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td>";
+	$teststr.="<td>%s</td><td>%s</td><td>%d</td><td>%s</td><td>%s</td><td>%s</td></tr>\n";
+	$sqldel="delete from blz_data";
 	$ok="true";
+	$cnt=0;
 	$f=fopen("blz.txt","r");
-	if ($test) echo "Testdurchlauf <br>";
+	if ($test) echo "Testdurchlauf <br><table>\n";
 	$i=0;
+	$start=time();
+	$rs = $db->getAll("SELECT current_setting('server_encoding')");
+	$srvencoding = $rs[0]['current_setting'];
+	$rs = $db->getAll("SELECT current_setting('client_encoding')");
+	$cliencoding = $rs[0]['current_setting'];
+	echo "SRV: $srvencoding - - CLI: $cliencoding<br>";
 	if ($f) {
-		if (!$test) $rc=$db->query("BEGIN");
+		//Cliententcoding auf Latin:
+		if (!$test) { $rc=$db->query("BEGIN"); if ($cliencoding=="UTF8") $db->query("SET CLIENT_ENCODING TO 'latin-9'"); };
 		if (!$test) $rc=$db->query($sqldel);
 		while (($zeile=fgets($f,256)) != FALSE) {
-			$sql=sprintf($sqlins,substr($zeile,0,8),substr($zeile,8,1),substr($zeile,9,58),substr($zeile,67,5),
+			$cnt++;
+			if (!$test){
+				//Datenfile ist immer Latin!!
+				//zwei Möglichkeiten der Zeichenwandlung. Was ist besser??
+				//Client nicht umgestellt, Zeichen wandeln
+				/*$sql=sprintf($sqlins,substr($zeile,0,8),substr($zeile,8,1),l2u(substr($zeile,9,58)),substr($zeile,67,5),
+						l2u(substr($zeile,72,35)),l2u(substr($zeile,107,27)),substr($zeile,134,5),substr($zeile,139,11),
+						substr($zeile,150,2),substr($zeile,152,6),substr($zeile,158,1),substr($zeile,159,1),
+						substr($zeile,160,8));*/
+				//Client umgestellt + und auch bei nicht UTF-Client:
+				$sql=sprintf($sqlins,substr($zeile,0,8),substr($zeile,8,1),substr($zeile,9,58),substr($zeile,67,5),
 						substr($zeile,72,35),substr($zeile,107,27),substr($zeile,134,5),substr($zeile,139,11),
 						substr($zeile,150,2),substr($zeile,152,6),substr($zeile,158,1),substr($zeile,159,1),
 						substr($zeile,160,8));
-			if (!$test){
 				$rc=$db->query($sql);
-				 if(DB::isError($rc)) {
-                        		echo $sql."<br><pre>";
+				if(DB::isError($rc)) {
+					echo $sql."<br><pre>";
 					echo $rc->getMessage()."</pre><br>";
 					$ok=false;
 					break;
 				}
+				if ($cnt % 10 == 0) { 
+					if ($cnt % 1000 == 0) { $x=time()-$start; echo sprintf("%dsec %6d<br>",$x,$cnt); }
+					else if ($cnt % 100 == 0) { echo "!"; }
+					else { echo '.'; }
+					flush(); 
+				}
+			} else {
+				echo sprintf($teststr,substr($zeile,0,8),substr($zeile,8,1),l2u(substr($zeile,9,58)),substr($zeile,67,5),
+                                                l2u(substr($zeile,72,35)),l2u(substr($zeile,107,27)),substr($zeile,134,5),substr($zeile,139,11),
+                                                substr($zeile,150,2),substr($zeile,152,6),substr($zeile,158,1),substr($zeile,159,1),
+                                                substr($zeile,160,8));
+				$rc=true;
+			}
+			if (!$rc) { 
+				$ok=false;
+				break;
 			}
 			$i++;
 		}
 		if ($ok) {
 			if (!$test) $rc=$db->query("COMMIT");
-			echo "$i Daten erfolgreich importiert";
+			echo "<br>$i Daten erfolgreich importierti<br>";
+			if ($cliencoding=="UTF8") $db->query("SET CLIENT_ENCODING TO 'UTF8'");
+			$stop=time();
+			echo $stop-$start." Sekunden";
 		} else {
 			if (!$test) $rc=$db->query("ROLLBACK");
 			echo "Fehler in Zeile: ".$i."<br>";
@@ -87,7 +138,8 @@ $db=$_SESSION["db"]; //new myDB($login);
 	} else {
 		ende(4);
 	}
-} 
+	echo "</table><br>Fertig. $i Banken importiert.";
+} else {
 ?>
 <p class="listtop">BLZ-Import f&uuml;r die ERP<p>
 <br>Die erste Zeile enth&auml;lt keine Feldnamen der Daten.<br>
@@ -95,7 +147,7 @@ Die Datenfelder haben eine feste Breite.<br><br>
 Die Daten k&ouml;nnen hier bezogen werden:<br>
 <a http='http://www.bundesbank.de/zahlungsverkehr/zahlungsverkehr_bankleitzahlen_download.php'>
 http://www.bundesbank.de/zahlungsverkehr/zahlungsverkehr_bankleitzahlen_download.php</a><br><br>
-ggf. das File vorher noch auf UTF8 wandeln: iconv -f latin1 -t  utf8 blz.txt -o blz1.txt<br><br>
+Das File vorher <b>nicht</b> auf UTF8 wandeln!<br><br>
 Achtung!! Die bestehenden BLZ-Daten werden zun&auml;chst gel&ouml;scht.
 <br>
 <form name="import" method="post" enctype="multipart/form-data" action="blz.php">
@@ -107,3 +159,4 @@ Achtung!! Die bestehenden BLZ-Daten werden zun&auml;chst gel&ouml;scht.
 <tr><td></td><td><input type="submit" name="ok" value="Import"></td></tr>
 </table>
 </form>
+<?php } ?>

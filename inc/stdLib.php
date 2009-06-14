ddcec5ae41208be0e836db3007cfcb5c36a93607
Jan Büren <information@richardson-bueren.de>
2009-06-14 10:57:31 +0000

    Was mir aber noch mehr am Herzen liegt: Nie wieder:  Syntaxfehler bei »Epine« von französischen Ansprechpartnern die im Wald leben (Bois de l'Epine). Ich find jeder sollte überall db->escapeSimple verwenden. Schont die Nerven und senkt die Fehleranzahl

--- inc/stdLib.php
+++ inc/stdLib.php
@@ -654,7 +654,7 @@ global $db;
 	$sql="insert into tempcsvdata (uid,csvdaten) values (";
 //	$sql.="'".$_SESSION["loginCRM"]."','".substr($tmpstr,0,-1)."')";
 	$sql.=	"'" . $_SESSION["loginCRM"]. "','"
-						  . $db->db->escapeSimple($tmpstr) . "')";	//erstmal korrekt escapen und den blöden substr weg. Sowas will ich nicht mehr sehen ;-)
+						  . $db->db->escapeSimple($tmpstr) . "')";		//erstmal korrekt escapen und den blöden substr weg.  Sowas will ich nicht mehr sehen ;-)
 	$rc=$db->query($sql);
 	return $rc;
 }

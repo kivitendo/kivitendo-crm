db919ea5a9388ae8b11bd9d0e562b857b2f990ef
Holger Lindemann <hli@lx-system.de>
2009-06-26 14:55:31 +0000

    Serienbrief Teil 2

--- mkserdocs.php
+++ mkserdocs.php
@@ -3,7 +3,7 @@ session_start();
 require_once("inc/stdLib.php");
 require_once("inc/crmLib.php");
 
-
+//Übersetzung der Platzhalter
 $hli2erp["P"]=array("ANREDE"=>"cp_greeting","TITEL"=>"cp_title","NAME1"=>"cp_name","NAME2"=>"cp_givenname",
                 "LAND"=>"cp_country","PLZ"=>"cp_zipcode","ORT"=>"cp_city","STRASSE"=>"cp_street",
                 "TEL"=>"cp_phone","FAX"=>"cp_fax","EMAIL"=>"cp_email","FIRMA"=>"name","GESCHLECHT"=>"cp_gender","ID"=>"cp_id");
@@ -13,11 +13,12 @@ $hli2erp["F"]=array("ANREDE"=>"greeting","NAME1"=>"name","NAME2"=>"department_1"
                 "USTID"=>"ustid","STEUERNR"=>"taxnumber","LANG"=>"language","KDTYP"=>"business_id",
                 "KTONR"=>"account_number","BANK"=>"bank","BLZ"=>"bank_code");
 
+//Bibliothek nachladen
 $typ=strtolower(substr($_SESSION["datei"],-3));
 switch ($typ) {
     case "tex" :
                 require('inc/phpTex.php');
-                $doc = new phpBIN();
+                $doc = new phpTex();
                 break;
     case "rtf" :
                 break;
@@ -25,9 +26,10 @@ switch ($typ) {
     case "swf" :
                 break;
 }
+
 echo "Bitte das Fenster erst nach Aufforderung schlie&szlig;en<br>";
 
-$doc->loadDocument("vorlage/".$_SESSION["datei"]);
+$doc->loadDocument("./dokumente/".$_SESSION["mansel"]."/serbrief/".$_SESSION["datei"]);
 $doc->savecontent();
 $sql="select * from tempcsvdata where uid = '".$_SESSION["loginCRM"]."' limit 1";
 $data=$db->getAll($sql);
@@ -41,10 +43,22 @@ foreach($felder as $value) {
     $vars[$name]="";
     $pos[$name]=$i++;
 }; 
-$data1["CRMUSER"]=$_SESSION["loginCRM"];
-$data1["cause"]=$_SESSION["SUBJECT"];
+
+//incCall vorbereiten
+$tdata["CRMUSER"]=$_SESSION["loginCRM"];
+$tdata["cause"]=$_SESSION["SUBJECT"];
+$tdata["c_cause"]=$_SESSION["BODY"];
+$tdata["Kontakt"]="S";
+$tdata["Bezug"]=0;
+$tdata["Zeit"]=date("H:i");
+$tdata["Datum"]=date("d.m.Y");
+$tdata["Status"]=1;
+$tdata["DateiID"]=$_SESSION["dateiId"];
+
+//Daten holen 
 $sql="select * from tempcsvdata where uid = ".$_SESSION["loginCRM"]." offset 1";
 $data=$db->getAll($sql);
+$cnt=1;
 if ($data) {
     foreach ($data as $row) {
         $tmp=split(":",$row["csvdaten"]);
@@ -52,12 +66,14 @@ if ($data) {
         $tmp[]=$_SESSION["SUBJECT"];
         $tmp[]=$_SESSION["BODY"];
         foreach($felder as $name) {
-            $nname=$hli2erp["P"][$name];
+            $nname=$hli2erp[$_SESSION["src"]][$name];
             $vars[$nname] = $tmp[$pos[$name]];
         }
+        $tdata["CID"]=$row["id"];
+        insCall($tdata,false);
         $doc->parse($vars);
         $doc->save($_SESSION["savefiledir"]."/".$row["id"]."_".$_SESSION["datei"]);
-        echo $row["id"]; flush();
+        if ($cnt++ % 10 == 0) echo "."; flush();
         $doc->getoriginal();
     }
 }
--- serdoc.php
+++ serdoc.php
@@ -1,21 +1,50 @@
 <?
 // $Id$
 session_start();
+include("inc/stdLib.php");
+require_once("documents.php");
+
+
+if ($_GET["src"]) $_POST["src"]=$_GET["src"];
+
 $filetype=array("rtf","tex","odt","swf");
+
 if ($_POST["erzeugen"]) {
     if (!empty($_FILES["datei"]["name"])) {
         $typ=strtolower(substr($_FILES["datei"]["name"],-3));
         if (in_array($typ,$filetype)) {
-            copy($_FILES["datei"]["tmp_name"],"vorlage/".$_FILES["datei"]["name"]);
+            //ok, gültige Datei angehängt, vorlage sichern
+            $dest="./dokumente/".$_SESSION["mansel"]."/serbrief";
+            $ok=chkdir("serbrief");
+            copy($_FILES["datei"]["tmp_name"],$dest."/".$_FILES["datei"]["name"]);
             unlink ($_FILES["datei"]["tmp_name"]);
+
+            //Verzeichnis anlegen für die Serienbriefe
             $savefiledir="serbrief/".substr($_FILES["datei"]["name"],0,-4);
             @mkdir($savefiledir);
-            $_SESSION["SUBJECT"]=$_POST["SUBJECT"];
+            
+            //Dokument in db speichern
+            $dbfile=new document();
+            $dbfile->setDocData("descript",$_POST["subject"]);
+            $dbfile->setDocData("pfad","serbrief");
+            $dbfile->setDocData("name",$_FILES["datei"]["name"]);
+            $dbfile->setDocData("descript",$_POST["body"]);
+            $rc=$dbfile->newDocument();
+            $dbfile->saveDocument();
+        
+            //benötigte Daten in Session speichern
+            $_SESSION["dateiId"] = $dbfile->id;
+            $_SESSION["SUBJECT"]=$_POST["subject"];
             $_SESSION["BODY"]=$_POST["body"];
             $_SESSION["DATE"]=$_POST["date"];
+            $_SESSION["src"]=$_POST["src"];
             $_SESSION["savefiledir"]=$savefiledir;
             $_SESSION["datei"]=$_FILES["datei"]["name"];
+            
+            //POP-Up erzeugt die Dokumente
             $js="f1=open('mkserdocs.php','SerDoc','width=600,height=100')";
+
+            //Formular nicht noch einmal sicken
             $display="hidden";
         } else {
             $js="alert('Ungültiger Dateityp')";
@@ -28,6 +57,7 @@ if ($_POST["erzeugen"]) {
 Daten f&uuml;r den Serienbrief:<br />
 <form name="serdoc" action="serdoc.php" enctype='multipart/form-data' method="post">
 <INPUT TYPE="hidden" name="MAX_FILE_SIZE" value="5000000">
+<input type="hidden" name="src" value="<?= $_POST["src"] ?>">
 Datum: <input type="text" name="date" size="12" value="<?= $_POST["date"] ?>"><br />
 Betreff: <input type="text" name="subject" size="30" value="<?= $_POST["subject"] ?>"><br />
 Zusatztext:<br />

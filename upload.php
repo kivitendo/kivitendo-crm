4ea2a6b37497c10e36eab29a444f5bd9ee85f0df
Holger Lindemann <hli@lx-system.de>
2010-02-27 18:49:47 +0100

    Multilingual, Dateisperre berücksichtigen

--- upload.php
+++ upload.php
@@ -5,8 +5,17 @@ include("inc/stdLib.php");
 if ($_POST) {
 	require_once("documents.php");
 	$dbfile=new document();
-	$dbfile->setDocData("descript",$_POST["descript"]);
-	$rc=$dbfile->uploadDocument($_FILES,$_POST["pfad"]);
+    $id=$dbfile->searchDocument($_FILES["Datei"]["name"],$_POST["pfad"]);
+    if ($id) {
+        $rc = $dbfile->getDokument($id);
+    }
+    if ($dbfile->lock>0) {
+        echo translate('.:file locked:.','firma')."!";    
+        $rc = False;
+    } else {
+	    $dbfile->setDocData("descript",$_POST["descript"]);
+	    $rc=$dbfile->uploadDocument($_FILES,$_POST["pfad"]);
+    }
 	if ($rc) {
 	?>
 	<script>
@@ -18,16 +27,16 @@ if ($_POST) {
 <link type="text/css" REL="stylesheet" HREF="css/main.css"></link>
 <head><body class="klein" style="padding:0em; margin:0em;">
 <table width="100%" class="klein lg">
-	<tr style="border-bottom:1px solid black;"><td><?= translate('Upload a new File','firma') ?></td><td align="right"><a href="javascript:top.main_window.newFile('left')">(X)</a></td></tr>
+	<tr style="border-bottom:1px solid black;"><td><?= translate('.:uploadDocument:.','firma') ?></td><td align="right"><a href="javascript:top.main_window.newFile('left')">(X)</a></td></tr>
 </table>
 <form name="iform" action="upload.php?fid=<?= $_GET["fid"] ?>&pid=<?= $_GET["pid"] ?>" method="post" enctype="multipart/form-data">
 <input id="upldpath" name="pfad" type="hidden">
 <br>
 &nbsp;<textarea name="descript" id="caption" cols="37" rows="3"></textarea><br>
-&nbsp;<?= translate('Remarks','firma') ?><br>
+&nbsp;<?= translate('.:Remarks:.','firma') ?><br>
 &nbsp;<input id="Datei" type="file" name="Datei" size="19"><br>
-&nbsp;<?= translate('Filename','firma') ?><br>
+&nbsp;<?= translate('.:Filename:.','firma') ?><br>
 <br>
-&nbsp;<input type="submit" value="<?= translate('save_','firma') ?>">
+&nbsp;<input type="submit" value="<?= translate('.:save:.','firma') ?>">
 </form>
 </html>

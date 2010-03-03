6d31268506f2eba8f34afb3b6bcf8824d4db948f
Holger Lindemann <hli@lx-system.de>
2010-03-03 16:55:16 +0100

    Fehlendes ergänzt, überflüssiges weg.

--- dokument.php
+++ dokument.php
@@ -12,6 +12,7 @@
 	$t->set_file(array("doc" => "dokument.tpl"));
 	$t->set_var(array(
 			AJAXJS  => $xajax->printJavascript(XajaxPath),
+            PICUP => "true",
 			));
 	$t->Lpparse("out",array("doc"),$_SESSION["lang"],"firma");
 

306351cacd66fca8254a76ef2e1ed8ba286d788a
Holger Lindeman <hli@mini.hoch.ul>
2010-12-01 14:58:39 +0100

    Reihenfolge der Kommandos

--- update/crm_OpportunityQuotation.sql
+++ update/crm_OpportunityQuotation.sql
@@ -2,9 +2,9 @@
 -- @description: Historie zu Auftragschancen + Auftragsnummer zuordnen
 
 ALTER TABLE opportunity ADD COLUMN oppid INT;
+UPDATE opportunity SET oppid = id;
 ALTER TABLE opportunity ALTER COLUMN oppid SET not null;
 ALTER TABLE opportunity ADD COLUMN auftrag INT;
 ALTER TABLE opportunity ALTER COLUMN auftrag SET DEFAULT 0;
 ALTER TABLE opportunity DROP COLUMN mtime;
-UPDATE opportunity SET oppid = id;
 

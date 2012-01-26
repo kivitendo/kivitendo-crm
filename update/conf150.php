bbe78cb8b0f4c144f863ad46169ee87182e9fb3a
Holger Lindemann <hli@lenny.hoch.ul>
2012-01-26 23:15:06 +0100

    Versionasnummer,
    Installanpassung

--- update/conf150.php
+++ update/conf150.php
@@ -5,4 +5,5 @@ $MailFlag='Flagged';
 $ttpart='azstd'; //Artikelnummer für Arbeitszeit
 $tttime=60; //Anzahl der Minuten für eine Einheit
 $ttround=15;    //Ab hier wird eine Einheit berechnet
+$logfile=false;
 ?>

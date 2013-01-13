a776cf043fa2a2d2c94d272a5c50f420f5a21da8
Ronny Kumke <ronny@lxcars.de>
2013-01-13 11:45:34 +0100

    Schnellsuche: Suchwort bleibt im Formular, Einige Fehler beseitigt
    Liefert die Suche nicht das gewünschte Resultat so muss nun nicht mehr das
    komplette Suchwort neu eingegeben werden. Bei Suchworten mit Leerzeichen
    wird nur der erste Teil ins Formular eingefügt.

--- getData.php
+++ getData.php
@@ -2,63 +2,62 @@
 ob_start(); 
 	require_once("inc/stdLib.php");
 	include("inc/crmLib.php");
-    $menu =  $_SESSION['menu'];
+    $menu = $_SESSION['menu'];
 ?>
 <html>
 <head><title></title>
-    <?php echo $menu['stylesheets']; ?>
-    <link type="text/css" REL="stylesheet" HREF="<?php echo $_SESSION['basepath'].'crm/css/'.$_SESSION["stylesheet"]; ?>/main.css"></link>
-    <?php  
-    echo '<link rel="stylesheet" type="text/css" href="'.$_SESSION['basepath'].'crm/jquery-ui/themes/base/jquery-ui.css"></link>'."\n"; 
-    echo   '<script type="text/javascript" src="'.$_SESSION['basepath'].'crm/jquery-ui/jquery.js"></script>'."\n"; 
-    echo   '<script type="text/javascript" src="'.$_SESSION['basepath'].'crm/jquery-ui/ui/jquery-ui.js"></script>'."\n"; 
+    <?php echo $menu['stylesheets'].'
+    <link type="text/css" REL="stylesheet" HREF="<'.$_SESSION["basepath"].'crm/css/'.$_SESSION["stylesheet"].'/main.css"></link>
+    <link rel="stylesheet" type="text/css" href="'.$_SESSION['basepath'].'crm/jquery-ui/themes/base/jquery-ui.css"></link> 
+    <script type="text/javascript" src="'.$_SESSION['basepath'].'crm/jquery-ui/jquery.js"></script> 
+    <script type="text/javascript" src="'.$_SESSION['basepath'].'crm/jquery-ui/ui/jquery-ui.js"></script>'; 
     if ($feature_ac) { 
-        
-        echo   '<style>
-                    .ui-autocomplete-category {
-                        font-weight: bold;
-                        padding: .2em .4em;
-                        margin: .8em 0 .2em;
-                        line-height: 1.5;
+        echo '   
+    <style>
+        .ui-autocomplete-category {
+            font-weight: bold;
+            padding: .2em .4em;
+            margin: .8em 0 .2em;
+            line-height: 1.5;
+        }
+    </style>
+    <script>
+        $.widget("custom.catcomplete", $.ui.autocomplete, {
+            _renderMenu: function(ul,items) {
+                var that = this,
+                currentCategory = "";
+                $.each( items, function( index, item ) {
+                    if ( item.category != currentCategory ) {
+                        ul.append( "<li class=\'ui-autocomplete-category\'>" + item.category + "</li>" );
+                        currentCategory = item.category;
                     }
-                </style>
-                <script>
-                    $.widget("custom.catcomplete", $.ui.autocomplete, {
-                        _renderMenu: function(ul,items) {
-                            var that = this,
-                            currentCategory = "";
-                            $.each( items, function( index, item ) {
-                                if ( item.category != currentCategory ) {
-                                    ul.append( "<li class=\'ui-autocomplete-category\'>" + item.category + "</li>" );
-                                    currentCategory = item.category;
-                                }
-                                that._renderItemData(ul,item);
-                            });
-                        }   
-                    });
-                </script> ';           
-        echo   '<script language="JavaScript"> 
-                    $(function() {
-                        $("#ac0").catcomplete({                          
-                            source: "ac.php?case=fastsearch",                            
-                            minLength: '.$feature_ac_minLength.',                            
-                            delay: '.$feature_ac_delay.',
-                            select: function(e,ui) {
-                                        $("#adress").click();
-                                    }
-                        });
-                    });
-                </script>'; 
+                    that._renderItemData(ul,item);
+                });
+             }
+         });     
+    </script>            
+    <script language="JavaScript"> 
+        $(function() {
+            $("#ac0").catcomplete({                          
+                source: "ac.php?case=fastsearch",                            
+                minLength: '.$feature_ac_minLength.',                            
+                delay: '.$feature_ac_delay.',
+                select: function(e,ui) {
+                    $("#adress").click();
+                }
+            });
+        });
+    </script>'; 
     }//end feature_ac 
     ?> 
     <script>
     $(function() {
-        $( "#dialog" ).dialog();
+        $("#dialog").dialog();
     });
     </script>
 </head>
-<body onLoad="document.suche.swort.focus()";>
-<?php
+<body onload=$("#ac0").focus().val('<?php echo preg_replace("#[ ].*#",'',$_GET['swort']);?>');>
+<?php //wichtig: focus().val('ohneLeerZeichen')
 echo $menu['pre_content'];
 echo $menu['start_content'];
 if ($_GET["adress"]) {
@@ -66,14 +65,14 @@ if ($_GET["adress"]) {
 	include("inc/persLib.php");
 	include("inc/UserLib.php");
 	//ToDo Dialogtexte verbessern?
-	$msg='<div id="dialog" title="Kein Suchwort">
-	            <p>Bitte geben Sie mindestens ein Zeichen ein.</br>Dialog muss nicht geschlossen werden.</p>
+	$msg='<div id="dialog" title="Kein Suchbegriff eingegeben">
+	            <p>Bitte geben Sie mindestens ein Zeichen ein.</p>
 	       </div>';
-	$viele='<div id="dialog" title="Zu viele Treffer">
-	            <p>Die Suche ergibt zu viele Resultate.</br> Bitte geben mehr Zeichen ein.</br>Dialog muss nicht geschlossen werden.</p>
+	$viele='<div id="dialog" title="Zu viele Suchergebnisse">
+	            <p>Die Suche ergibt zu viele Resultate.</br> Bitte geben mehr Zeichen ein.</p>
 	       </div>';
-	$keine='<div id="dialog" title="Keine Treffer">
-                <p>Die Suche ergibt kein Resultat.</br>Schreibweise überprüfen!</br>Dialog muss nicht geschlossen werden.</p>
+	$keine='<div id="dialog" title="Nichts gefunden">
+                <p>Dieser Suchbegriff ergibt kein Resultat.</br>Bitte überprüfen Sie die Schreibweise!</p>
             </div>';
 	$found=false;
 	$suchwort=mkSuchwort($_GET["swort"]);
@@ -103,54 +102,54 @@ if ($_GET["adress"]) {
 <script language="JavaScript">
 <!--
 	function showD (src,id) {
-		if      (src=="C") {	uri="firma1.php?Q=C&id=" + id }
-		else if (src=="V") {	uri="firma1.php?Q=V&id=" + id; }
-		else if (src=="E") {	uri="user1.php?id=" + id; }
-		else if (src=="K") {	uri="kontakt.php?id=" + id; }
+		if      (src=="C") { uri="firma1.php?Q=C&id=" + id }
+		else if (src=="V") { uri="firma1.php?Q=V&id=" + id; }
+		else if (src=="E") { uri="user1.php?id=" + id; }
+		else if (src=="K") { uri="kontakt.php?id=" + id; }
 		window.location.href=uri;
 	}
 //-->
 </script>
 <?php
 	if ($anzahl>0) {
-            if ($anzahl==1 && $rsC) header("Location: firma1.php?Q=C&id=".$rsC[0]['id']); 
-            if ($anzahl==1 && $rsV) header("Location: firma1.php?Q=V&id=".$rsV[0]['id']); 
-            if ($anzahl==1 && $rsK) header("Location: kontakt.php?id=".$rsK[0]['id']); 
-            if ($anzahl==1 && $rsE) header("Location: user.php?id=".$rsE[0]['id']); 
-            echo '<p class="listtop">Suchergebnis</p>'; 
-            echo "<table class=\"liste\">\n"; 
-            echo "<tr class='bgcol3'><th>KD-Nr</th><th class=\"liste\">Name</th><th class=\"liste\">Anschrift</th><th class=\"liste\">Telefon</th><th></th></tr>\n"; 
-            $i=0; 
-            if ($rsC) foreach($rsC as $row) { 
-                echo "<tr onMouseover=\"this.bgColor='#FF0000';\" onMouseout=\"this.bgColor='".$bgcol[($i%2+1)]."';\" bgcolor='".$bgcol[($i%2+1)]."' onClick='showD(\"C\",".$row["id"].");'>". 
-                             "<td class=\"liste\">".$row["customernumber"]."</td><td class=\"liste\">".$row["name"]."</td>". 
-                                     "<td class=\"liste\">".$row["city"].(($row["street"])?",":"").$row["street"]."</td><td class=\"liste\">".$row["phone"]."</td><td class=\"liste\">K</td></tr>\n"; 
-                $i++; 
-            }  
-            if ($rsV) foreach($rsV as $row) { 
-                echo "<tr onMouseover=\"this.bgColor='#FF0000';\" onMouseout=\"this.bgColor='".$bgcol[($i%2+1)]."';\" bgcolor='".$bgcol[($i%2+1)]."' onClick='showD(\"V\",".$row["id"].");'>". 
-                             "<td class=\"liste\">".$row["vendornumber"]."</td><td class=\"liste\">".$row["name"]."</td>". 
-                                     "<td class=\"liste\">".$row["city"].(($row["street"])?",":"").$row["street"]."</td><td class=\"liste\">".$row["phone"]."</td><td class=\"liste\">L</td></tr>\n"; 
-                $i++; 
-            } 
-            if ($rsK) foreach($rsK as $row) { 
-                echo "<tr onMouseover=\"this.bgColor='#FF0000';\" onMouseout=\"this.bgColor='".$bgcol[($i%2+1)]."';\" bgcolor='".$bgcol[($i%2+1)]."' onClick='showD(\"K\",".$row["cp_id"].");'>". 
-                                     "<td class=\"liste\">".$row["cp_id"]."</td><td class=\"liste\">".$row["cp_name"].", ".$row["cp_givenname"]."</td>". 
-                                     "<td class=\"liste\">".$row["addr2"].(($row["addr1"])?",":"").$row["addr1"]."</td><td class=\"liste\">".$row["cp_phone1"]."</td><td class=\"liste\">P</td></tr>\n"; 
-                $i++; 
-            } 
-            if ($rsE) foreach($rsE as $row) { 
-                echo "<tr onMouseover=\"this.bgColor='#FF0000';\" onMouseout=\"this.bgColor='".$bgcol[($i%2+1)]."';\" bgcolor='".$bgcol[($i%2+1)]."' onClick='showD(\"E\",".$row["id"].");'>". 
-                                     "<td class=\"liste\">".$row["id"]."</td><td class=\"liste\">".$row["name"]."</td>". 
-                                     "<td class=\"liste\">".$row["addr2"].(($row["addr1"])?",":"").$row["addr1"]."</td><td class=\"liste\">".$row["workphone"]."</td><td class=\"liste\">U</td></tr>\n"; 
-                $i++; 
-            } 
-            echo "</table>\n";
+        if ($anzahl==1 && $rsC) header("Location: firma1.php?Q=C&id=".$rsC[0]['id']); 
+        if ($anzahl==1 && $rsV) header("Location: firma1.php?Q=V&id=".$rsV[0]['id']); 
+        if ($anzahl==1 && $rsK) header("Location: kontakt.php?id=".$rsK[0]['id']); 
+        if ($anzahl==1 && $rsE) header("Location: user.php?id=".$rsE[0]['id']); 
+        echo '<p class="listtop">Suchergebnis</p>'; 
+        echo "<table class=\"liste\">\n"; 
+        echo "<tr class='bgcol3'><th>KD-Nr</th><th class=\"liste\">Name</th><th class=\"liste\">Anschrift</th><th class=\"liste\">Telefon</th><th></th></tr>\n"; 
+        $i=0; 
+        if ($rsC) foreach($rsC as $row) { 
+            echo "<tr onMouseover=\"this.bgColor='#FF0000';\" onMouseout=\"this.bgColor='".$bgcol[($i%2+1)]."';\" bgcolor='".$bgcol[($i%2+1)]."' onClick='showD(\"C\",".$row["id"].");'>". 
+                 "<td class=\"liste\">".$row["customernumber"]."</td><td class=\"liste\">".$row["name"]."</td>". 
+                 "<td class=\"liste\">".$row["city"].(($row["street"])?",":"").$row["street"]."</td><td class=\"liste\">".$row["phone"]."</td><td class=\"liste\">K</td></tr>\n"; 
+            $i++; 
+        }  
+        if ($rsV) foreach($rsV as $row) { 
+            echo "<tr onMouseover=\"this.bgColor='#FF0000';\" onMouseout=\"this.bgColor='".$bgcol[($i%2+1)]."';\" bgcolor='".$bgcol[($i%2+1)]."' onClick='showD(\"V\",".$row["id"].");'>". 
+                 "<td class=\"liste\">".$row["vendornumber"]."</td><td class=\"liste\">".$row["name"]."</td>". 
+                 "<td class=\"liste\">".$row["city"].(($row["street"])?",":"").$row["street"]."</td><td class=\"liste\">".$row["phone"]."</td><td class=\"liste\">L</td></tr>\n"; 
+            $i++; 
+        } 
+        if ($rsK) foreach($rsK as $row) { 
+            echo "<tr onMouseover=\"this.bgColor='#FF0000';\" onMouseout=\"this.bgColor='".$bgcol[($i%2+1)]."';\" bgcolor='".$bgcol[($i%2+1)]."' onClick='showD(\"K\",".$row["cp_id"].");'>". 
+                 "<td class=\"liste\">".$row["cp_id"]."</td><td class=\"liste\">".$row["cp_name"].", ".$row["cp_givenname"]."</td>". 
+                 "<td class=\"liste\">".$row["addr2"].(($row["addr1"])?",":"").$row["addr1"]."</td><td class=\"liste\">".$row["cp_phone1"]."</td><td class=\"liste\">P</td></tr>\n"; 
+            $i++; 
+        } 
+        if ($rsE) foreach($rsE as $row) { 
+            echo "<tr onMouseover=\"this.bgColor='#FF0000';\" onMouseout=\"this.bgColor='".$bgcol[($i%2+1)]."';\" bgcolor='".$bgcol[($i%2+1)]."' onClick='showD(\"E\",".$row["id"].");'>". 
+                 "<td class=\"liste\">".$row["id"]."</td><td class=\"liste\">".$row["name"]."</td>". 
+                 "<td class=\"liste\">".$row["addr2"].(($row["addr1"])?",":"").$row["addr1"]."</td><td class=\"liste\">".$row["workphone"]."</td><td class=\"liste\">U</td></tr>\n"; 
+            $i++; 
+        } 
+        echo "</table>\n";
         } else { 
  	        echo $msg; 
         }; 
- 	echo "<br>"; 
-} else if ($_GET["kontakt"]){
+        echo "<br>"; 
+    } else if ($_GET["kontakt"]) {
 ?>
 <script language="JavaScript">
 	sw="<?php echo  $_GET["swort"]; ?>";
@@ -158,14 +157,15 @@ if ($_GET["adress"]) {
 		F1=open("suchKontakt.php?suchwort="+sw+"&Q=S","Suche","width=400, height=400, left=100, top=50, scrollbars=yes");
 </script>			
 
-<?php }
-echo $menu['end_content'];
-ob_end_flush(); 
-?> 
+<?php } ?> 
 <p class="listtop">Schnellsuche Kunde/Lieferant/Kontakte und Kontaktverlauf</p>
-	<form name="suche" action="getData.php" method="get">
-	<input type="text" name="swort" size="25" id="ac0" autocomplete="off"> suche 
-	<input type="submit" name="adress" value="Adresse" id="adress">
-	<input type="submit" name="kontakt" value="Kontaktverlauf"> <br>
-      	<span class="liste">Suchbegriff</span>
-	</form>
+<form name="suche" action="getData.php" method="get">
+    <input type="text" name="swort" size="25" id="ac0" autocomplete="off"> suche 
+    <input type="submit" name="adress" value="Adresse" id="adress">
+    <input type="submit" name="kontakt" value="Kontaktverlauf"> <br>
+    <span class="liste">Suchbegriff</span>
+</form>
+<?php
+    echo $menu['end_content'];
+    ob_end_flush(); 
+?>

f046c73b3f6dfd55b958396040ade8e3f679a1c2
Holger Lindemann <hli@lx-system.de>
2013-01-14 13:33:41 +0100

    Bei mehr als 11 Treffer nicht abbrechen, sondern nur die ersten 11 übernehmen.
    For der forschleife $rs auf Treffer prüfen.

--- ac.php
+++ ac.php
@@ -12,17 +12,21 @@ if ($_GET['case']=='fastsearch') {
     $rsV = getAllFirmen($suchwort,true,"V"); 
     $rsK = getAllPerson($suchwort); 
     $rs = array(); 
-    foreach ( $rsC as $key => $value ) { 
-        if (count($rs) > 11) break;
-        array_push($rs,array('label'=>$value['name'],'category'=>'')); 
+    $cnt = 0;
+    if ($rsC) foreach ( $rsC as $key => $value ) { 
+        if ($cnt > 11) break;
+        array_push($rs,array('label'=>$value['name'],'category'=>'Kunden')); 
+        $cnt++;
     } 
-    foreach ( $rsV as $key => $value ) {
-        if (count($rs) > 11) break; 
+    if ($rsV) foreach ( $rsV as $key => $value ) {
+        if ($cnt > 11) break;
         array_push($rs,array('label'=>$value['name'],'category'=>'Lieferanten'));//ToDo translate 
+        $cnt++;
     } 
-    foreach ( $rsK as $key => $value ) {
-        if (count($rs) > 11) break;  
+    if ($rsK) foreach ( $rsK as $key => $value ) {
+        if ($cnt > 11) break;
         array_push($rs,array('label'=>$value['cp_givenname']." ".$value['cp_name'],'category'=>'Personen'));//ToDo translate 
+        $cnt++;
     } 
     echo json_encode($rs); 
 } 

f2f35500194a256b99f60c00493ee8a09a9e8615
Ronny Kumke <ronny@lxcars.de>
2013-01-16 07:11:55 +0100

    Revert "Bei mehr als 11 Treffer nicht abbrechen, sondern nur die ersten 11 übernehmen."
    This reverts commit f046c73b3f6dfd55b958396040ade8e3f679a1c2.

--- ac.php
+++ ac.php
@@ -12,21 +12,17 @@ if ($_GET['case']=='fastsearch') {
     $rsV = getAllFirmen($suchwort,true,"V"); 
     $rsK = getAllPerson($suchwort); 
     $rs = array(); 
-    $cnt = 0;
-    if ($rsC) foreach ( $rsC as $key => $value ) { 
-        if ($cnt > 11) break;
-        array_push($rs,array('label'=>$value['name'],'category'=>'Kunden')); 
-        $cnt++;
+    foreach ( $rsC as $key => $value ) { 
+        if (count($rs) > 11) break;
+        array_push($rs,array('label'=>$value['name'],'category'=>'')); 
     } 
-    if ($rsV) foreach ( $rsV as $key => $value ) {
-        if ($cnt > 11) break;
+    foreach ( $rsV as $key => $value ) {
+        if (count($rs) > 11) break; 
         array_push($rs,array('label'=>$value['name'],'category'=>'Lieferanten'));//ToDo translate 
-        $cnt++;
     } 
-    if ($rsK) foreach ( $rsK as $key => $value ) {
-        if ($cnt > 11) break;
+    foreach ( $rsK as $key => $value ) {
+        if (count($rs) > 11) break;  
         array_push($rs,array('label'=>$value['cp_givenname']." ".$value['cp_name'],'category'=>'Personen'));//ToDo translate 
-        $cnt++;
     } 
     echo json_encode($rs); 
 } 

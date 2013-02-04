01705b4033baf3df42f97a204ba13380e986dfb5
Holger Lindemann <hli@lx-system.de>
2013-02-04 09:05:20 +0100

    Minimalinstallation

--- terminmonat.php
+++ terminmonat.php
@@ -6,8 +6,8 @@
 ?>
 <html onLoad="self.focus()">
     <head><title></title>
-    <link type="text/css" REL="stylesheet" HREF="<?php echo $_SESSION['basepath'].'css/'.$_SESSION["stylesheet"]; ?>/main.css"></link>
     <?php echo $menu['stylesheets']; ?>
+    <link type="text/css" REL="stylesheet" HREF="<?php echo $_SESSION['basepath'].'css/'.$_SESSION["stylesheet"]; ?>/main.css"></link>
 	<script language="JavaScript">
 	<!--
 <?php

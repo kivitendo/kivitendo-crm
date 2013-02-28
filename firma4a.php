190e3a73fced6272758fcf668574b746da4e8049
Holger Lindemann <hli@lx-system.de>
2013-02-28 11:21:14 +0100

    doHeader benutzen
    Anpassung Dokumenterstellung

--- firma4a.php
+++ firma4a.php
@@ -3,20 +3,17 @@
     include("crmLib.php");
     include("FirmenLib.php");
     require_once("documents.php");
+    include("template.inc");
+    include("persLib.php");
+
+    $t = new Template($base);
+    doHeader($t);
     $pid=($_GET["pid"])?$_GET["pid"]:$_POST["pid"];
     $fid=($_GET["fid"])?$_GET["fid"]:$_POST["fid"];
     $did=($_GET["did"])?$_GET["did"]:$_POST["did"];
     $tab=($_GET["tab"])?$_GET["tab"]:$_POST["tab"];
-    
-    if ($fid) {
-        $fa=getFirmenStamm($fid,true,$tab);
-        $pfad="/$tab";
-        $pfad.=($tab=="C")?$fa["customernumber"]:$fa["vendornumber"];
-    };
-    if ($pid) $pfad.="/$pid";
-    $knopf="<input type='submit' name='erstellen' value='Erstellen'>";
-    if ($_POST["erstellen"]) {
-        $docdata=getDOCvorlage($_POST["docid"]);
+    if ( $_POST['erstellen'] ) {
+        $docdata=getDOCvorlage($_POST["did"]);
         if ($docdata["document"]["applikation"]=="O") {
             include('inc/phpOpenOffice.php');
             $doc = new phpOpenOffice();
@@ -31,7 +28,7 @@
             $doc = new phpBIN();
         }
         $fname=$docdata["document"]["file"];
-        $doc->loadDocument("vorlage/".$docdata["document"]["file"]);
+        $doc->loadDocument("./vorlage/".$docdata["document"]["file"]);
         $vars= array();
         if ($docdata["felder"]) foreach($docdata["felder"] as $zeile) {
             $value=$zeile["platzhalter"];
@@ -43,19 +40,21 @@
             }
         }
         $doc->parse($vars);
-        $data=$_POST;
         $name=date("YmdHi").$fname;
         $doc->prepsave("$name");
+        $pfad = $_POST['pfad'];
         copy('tmp/'.$name,"dokumente/".$_SESSION["mansel"]."$pfad/$name");
         $dbfile=new document();
         $dbfile->setDocData("descript","Dokumentvorlage: ".$docdata["document"]["vorlage"]."\n".$docdata["document"]["beschreibung"]);
         $dbfile->setDocData("name",$name);
         $dbfile->setDocData("pfad",$pfad);
         $rc=$dbfile->saveDocument();
-        $doc->clean();
+        //$doc->clean();
         $cdata["id"]=mknewTelCall();
         $cdata["Datum"]=date("d.m.Y");
+        $cdata["Date"]=date("d.m.Y");
         $cdata["Zeit"]=date("H:i");
+        $cdata["Time"]=date("H:i");
         $cdata["datei"]=1;
         $cdata["cause"]=$docdata["document"]["vorlage"];
         $cdata["c_cause"]=$docdata["document"]["beschreibung"];
@@ -65,13 +64,16 @@
         $cdata["CRMUSER"]=$_SESSION["loginCRM"];
         updCall($cdata);
         documenttotc($cdata["id"],$dbfile->id);
-        //$knopf="Dokument erstellt: <a href='$pfad/$name'>&lt;shift&gt;+&lt;klick&gt;</a>";
-        echo "<script language='JavaScript'>top.main_window.dateibaum('left','$pfad');top.main_window.showFile('left','$name');</script>";
-        exit;
-    }
+        echo "Dokument erstellt"; 
+        exit();
+    };
+    if ($fid) {
+        $fa=getFirmenStamm($fid,true,$tab);
+        $pfad="/$tab";
+        $pfad.=($tab=="C")?$fa["customernumber"]:$fa["vendornumber"];
+    };
+    if ($pid) $pfad.="/$pid";
 
-    include("template.inc");
-    include("persLib.php");
     if (empty($fid)) {
         $data=getKontaktStamm($pid);
         $data["ANREDE"]=$data["cp_greeting"]." ".$data["cp_title"];
@@ -109,46 +111,51 @@
             $art="Firmendokumente";
         }
     }
-    $document=getDocVorlage($did);
-    $t = new Template($base);
+    $document = getDocVorlage($did);
     $t->set_file(array("doc" => "firma4a.tpl"));
     $t->set_var(array(
             FAART => ($tab=="C")?"Kunde":"Lieferant",
-            ERPCSS      => $_SESSION['basepath'].'crm/css/'.$_SESSION["stylesheet"],
             TAB => $tab,
+            PFAD => $pfad,
             FID => $fid,
             PID => $pid,
             Art => $art,
             Beschreibung => $document["document"]["beschreibung"],
             DOCID => $did,
-            Knopf => $knopf
             ));
     $t->set_block("doc","Liste","Block");
     $t->set_block("doc","RegEx","Block2");
     $i=0;
     $data["DATUM"]=date("d.m.Y");
+    $data["DATE"]=date("d.m.Y");
     $data["ZEIT"]=date("H:i");
+    $data["Time"]=date("H:i");
     if ($document["felder"]) {
-     foreach($document["felder"] as $zeile) {
-        $value=$zeile["platzhalter"];
-        if ($zeile["laenge"]>60) {
-            $rows=floor($zeile["laenge"]/60)+1;
-            $input="<textarea class='klein' cols=60 rows=$rows name='".$zeile["platzhalter"]."'>".$data[$value]."</textarea>";
-        } else {
-            $input="<input type='text' name='".$zeile["platzhalter"]."' size='".$zeile["laenge"]."' value='".$data[$value]."'>";
+        foreach($document["felder"] as $zeile) {
+            $value=$zeile["platzhalter"]; //Alte Platzhalter
+            if ( !array_key_exists($value,$data) ) $value=strtolower($zeile["platzhalter"]); //DB-Namen
+            if ($zeile["laenge"]>60) {
+                $rows=floor($zeile["laenge"]/60)+1;
+                $input="<textarea class='klein' cols=60 rows=$rows name='".$zeile["platzhalter"]."'>".$data[$value]."</textarea>";
+                $typ="textarea";
+            } else {
+                $input="<input type='text' name='".$zeile["platzhalter"]."' size='".$zeile["laenge"]."' value='".$data[$value]."'>";
+                $typ="input";
+            }
+            $t->set_var(array(
+                EINGABE => $input,
+                Feldname => $zeile["feldname"]
+            ));
+            $t->parse("Block","Liste",true);
+            $t->set_var(array(
+                fld => $zeile["platzhalter"],
+                typ => $typ,
+                fldname => $zeile["feldname"],
+                regul => $zeile["zeichen"]
+            ));
+            $t->parse("Block2","RegEx",true);
+            $i++;
         }
-        $t->set_var(array(
-            EINGABE => $input,
-            Feldname => $zeile["feldname"]
-        ));
-        $t->parse("Block","Liste",true);
-        $t->set_var(array(
-            fld => $zeile["platzhalter"],
-            regul => $zeile["zeichen"]
-        ));
-        $t->parse("Block2","RegEx",true);
-        $i++;
-     }
     }
     $t->Lpparse("out",array("doc"),$_SESSION["lang"],"firma");
 ?>

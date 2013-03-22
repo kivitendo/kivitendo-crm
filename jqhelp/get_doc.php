a403f45f4950c372f9bc062d81b55213adc49030
Ronny Kumke <ronny@lxcars.de>
2013-03-22 16:46:40 +0100

    Fehler Link Aufträge beseitigt

--- jqhelp/get_doc.php
+++ jqhelp/get_doc.php
@@ -1,29 +1,28 @@
-    <script>
-    function OpenIOQ (src,id,type) {
-        if      (src=="C"&&type=="inv") { uri="../is.pl?action=edit&id=" + id }
-        else if (src=="V"&&type=="inv") { uri="../ap.pl?action=edit&id=" + id }
-        else if (src=="C"&&type=="quo") { uri="..oe.pl?action=edit&type=sales_quotation&vc=customer&id=" + id }
-	    else if (src=="V"&&type=="inv") { uri="../oe.pl?action=edit&type=sales_quotation&vc=vendor&id=" + id }
-	    else if (src=="C"&&type=="inv") { uri="../oe.pl?action=edit&type=sales_order&vc=customer&id=" + id }
-	    else if (src=="V"&&type=="inv") { uri="../oe.pl?action=edit&type=sales_order&vc=vendor&id=" + id }
-        window.location.href=uri;
+<script>
+	function OpenIOQ (src,id,type) {
+		if 	   (src=="C"&&type=="inv") { uri="../is.pl?action=edit&id=" + id }
+		else if (src=="V"&&type=="inv") { uri="../ap.pl?action=edit&id=" + id }
+		else if (src=="C"&&type=="quo") { uri="../oe.pl?action=edit&type=sales_quotation&vc=customer&id=" + id }
+		else if (src=="V"&&type=="quo") { uri="../oe.pl?action=edit&type=sales_quotation&vc=vendor&id=" + id }
+		else if (src=="C"&&type=="ord") { uri="../oe.pl?action=edit&type=sales_order&vc=customer&id=" + id }
+		else if (src=="V"&&type=="ord") { uri="../oe.pl?action=edit&type=sales_order&vc=vendor&id=" + id }
+     	window.location.href=uri;
 	}
-    $(document).ready(
-        $(function() {
-            $("#result_ioq")
-                .tablesorter({widthFixed: true, widgets: ['zebra']})
-                .tablesorterPager({container: $("#pager"), size: 20, positionFixed: false})
-        })
-    );  
-    </script>
+	$(document).ready(
+   	$(function() {
+      	$("#result_ioq")
+         	.tablesorter({widthFixed: true, widgets: ['zebra']})
+            .tablesorterPager({container: $("#pager"), size: 20, positionFixed: false})
+		})
+	);  
+</script>
 
 <?php
 /****************************************************************************************
 *** Erzeugt eine Tabelle mit offenen bzw. allen Aufträgen, Angeboten oder Rechungen   ***
 ****************************************************************************************/
 
-//ToDo: bgCol,  LimitAnzahl, klickbar!!
-
+//ToDo: bgCol, Tablesorter reparieren
 
 include ("../inc/crmLib.php");
 include ("../inc/stdLib.php");
@@ -38,20 +37,19 @@ if ($rs)
              "<td class=\"liste\">".$row["amount"]."</td><td class=\"liste\">".$row["number"]."</td></tr>\n"; 
         $i++; 
     } 
-    echo "</tbody></table>\n"; 
+echo "</tbody></table>\n"; 
 ?>
 <br>
-    <div id="pager" class="pager" style='position:absolute;'>
-        <img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/first.png" class="first"/>
-        <img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/prev.png" class="prev"/>
-        <img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/next.png" class="next"/>
-        <img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/last.png" class="last"/>
-        <select class="pagesize" id='pagesize'>
+<div id="pager" class="pager" style='position:absolute;'>
+	<img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/first.png" class="first"/>
+  	<img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/prev.png" class="prev"/>
+   <img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/next.png" class="next"/>
+   <img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/last.png" class="last"/>
+   <select class="pagesize" id='pagesize'>
         <option value="10">10</option>
         <option value="15" selected>15</option>
         <option value="20">20</option>
         <option value="25">25</option>
         <option value="30">30</option>
-        </select>
-        </form>
-    </div>    
+	</select>
+</div>    

9dd58b120f3def607c4da468de651f6e0ff041ef
Ronny Kumke <ronny@lxcars.de>
2013-05-09 11:02:39 +0200

    Angebote, Aufträge, Rechnungen in Kunden / Lieferantenmaske:
    Erste Position repariert, Einkaufsrechnunen, Lierantenauftrag, Preisanfragen
    ToDo: Tablesorter reparieren (funzt nur nach dem erstem Klick)

--- jqhelp/get_doc.php
+++ jqhelp/get_doc.php
@@ -1,6 +1,6 @@
 <script>
 	function OpenIOQ (src,id,type) {
-		if 	   (src=="C"&&type=="inv") { uri="../is.pl?action=edit&id=" + id }
+		if 	     (src=="C"&&type=="inv") { uri="../is.pl?action=edit&id=" + id }
 		else if (src=="V"&&type=="inv") { uri="../ap.pl?action=edit&id=" + id }
 		else if (src=="C"&&type=="quo") { uri="../oe.pl?action=edit&type=sales_quotation&vc=customer&id=" + id }
 		else if (src=="V"&&type=="quo") { uri="../oe.pl?action=edit&type=sales_quotation&vc=vendor&id=" + id }
@@ -22,7 +22,8 @@
 *** Erzeugt eine Tabelle mit offenen bzw. allen Aufträgen, Angeboten oder Rechungen   ***
 ****************************************************************************************/
 
-//ToDo: bgCol, Tablesorter reparieren
+//!!!!!!!!!ToDo: bgCol, Tablesorter reparieren
+// Warum funzt der TableSorter nicht mehr nach dem zweitem Klick auf einem der Reiter????
 
 include ("../inc/crmLib.php");
 include ("../inc/stdLib.php");
@@ -44,11 +45,11 @@ echo "</tbody></table>\n";
 <div id="pager" class="pager" style='position:absolute;'>
 	<img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/first.png" class="first"/>
   	<img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/prev.png" class="prev"/>
-   <img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/next.png" class="next"/>
-   <img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/last.png" class="last"/>
-   <select class="pagesize" id='pagesize'>
-        <option value="10">10</option>
-        <option value="15" selected>15</option>
+    <img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/next.png" class="next"/>
+    <img src="<?php echo $_SESSION['baseurl']; ?>crm/jquery-ui/plugin/Table/addons/pager/icons/last.png" class="last"/>
+    <select class="pagesize" id='pagesize'>
+        <option value="10" selected>10</option>
+        <option value="15">15</option>
         <option value="20">20</option>
         <option value="25">25</option>
         <option value="30">30</option>

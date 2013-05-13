fdb519913b4654f8b0cec1564e022d7d88fb9e15
Holger Lindemann <hli@lx-system.de>
2013-05-13 11:25:45 +0200

    Downloadlink korrigiert

--- download.php
+++ download.php
@@ -8,7 +8,7 @@
 <meta http-equiv="refresh" content="1; URL='<?php echo  $file ?>'">
 </head>
 <body onLoad="self.focus();">
-<div align="center"><a href="<?php echo  $_GET['file'] ?>">Der Download startet sofort.<br \> 
+<div align="center"><a href="<?php echo  "dokumente/".$_SESSION["mansel"].$_GET['file'] ?>">Der Download startet sofort.<br \> 
 Falls Ihr Browser die automatische Weiterleitung nicht unterst&uuml;tzt klicken Sie hier!</a><br><br>
 <a href="JavaScript:self.close();">close</a>
 </div>

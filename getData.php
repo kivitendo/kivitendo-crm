5cc6af15606d28bbf6157107a0be30a9596de2b4
Holger Lindemann <hli@lx-system.de>
2013-06-27 17:24:18 +0200

    $db -> $_SESSION
    Reihefolge der Abfrage
    Verzicht auf chkAnzahl
    class="liste" raus

--- getData.php
+++ getData.php
@@ -1,7 +1,7 @@
 <?php
 ob_start(); 
-	require_once("inc/stdLib.php");
-	include("inc/crmLib.php");
+    require_once("inc/stdLib.php");
+    include("inc/crmLib.php");
     $menu = $_SESSION['menu'];
     $head = mkHeader();
 ?>
@@ -16,18 +16,17 @@ ob_start();
 <?php echo $head['JUI-DROPDOWN']; ?>
     <script language="JavaScript">
         function showD (src,id) {
-	       if      (src=="C") { uri="firma1.php?Q=C&id=" + id }
-		   else if (src=="V") { uri="firma1.php?Q=V&id=" + id; }
-		   else if (src=="E") { uri="user1.php?id=" + id; }
-  		   else if (src=="K") { uri="kontakt.php?id=" + id; }
-		   window.location.href=uri;
-	    }
+           if      (src=="C") { uri="firma1.php?Q=C&id=" + id }
+           else if (src=="V") { uri="firma1.php?Q=V&id=" + id; }
+           else if (src=="E") { uri="user1.php?id=" + id; }
+           else if (src=="K") { uri="kontakt.php?id=" + id; }
+           window.location.href=uri;
+        }
         function showItem(id,Q,FID) {
-		    F1=open("<?php echo $_SESSION["basepath"]; ?>crm/getCall.php?Q="+Q+"&fid="+FID+"&hole="+id,"Caller","width=610, height=600, left=100, top=50, scrollbars=yes");
-	    }        
+            F1=open("<?php echo $_SESSION["basepath"]; ?>crm/getCall.php?Q="+Q+"&fid="+FID+"&hole="+id,"Caller","width=610, height=600, left=100, top=50, scrollbars=yes");
+        }        
     </script> 
 <?php    
-//print_r($_SESSION);
 if ($_SESSION['feature_ac']) { //funktioniert wegen der Ersetzungen für minLength und delay nur mit echo
     echo '
     <style>
@@ -69,7 +68,7 @@ if ($_SESSION['feature_ac']) { //funktioniert wegen der Ersetzungen für minLeng
 ?>  
 <style>
     table.tablesorter {
-	   width: 900;
+       width: 900;
     }  
     #jui_dropdown {
         height: 400px;
@@ -82,7 +81,6 @@ if ($_SESSION['feature_ac']) { //funktioniert wegen der Ersetzungen für minLeng
         display: inline-block;
         list-style: none;
     }   
-
     .drop_container {
         margin: 10px 10px 10px 10px ;
         display: inline-block;
@@ -141,81 +139,86 @@ echo '<div id="drop">
   <ul id="menu"> </ul>';
 
 if (isset($_GET["kontakt"]) && $_GET['swort'] != '') { 
-	$sw = strtoupper( $_GET["suchwort"] );
-	$sw = strtr( $sw, "*?", "%_" );
-	$sql  = "select calldate,cause,t.id,caller_id,bezug,V.name as lname,C.name as kname,P.cp_name as pname ";
-	$sql .= "from telcall t left join customer C on C.id=caller_id left join vendor V on V.id=caller_id ";
-	$sql .= "left join contacts P on caller_id=P.cp_id where UPPER(cause) like '%$sw%' or UPPER(c_long) like '%$sw%' ";
+    $sw = strtoupper( $_GET["swort"] );
+    $sw = strtr( $sw, "*?", "%_" );
+    $sql  = "select calldate,cause,t.id,caller_id,bezug,V.name as lname,C.name as kname,P.cp_name as pname ";
+    $sql .= "from telcall t left join customer C on C.id=caller_id left join vendor V on V.id=caller_id ";
+    $sql .= "left join contacts P on caller_id=P.cp_id where UPPER(cause) like '%$sw%' or UPPER(c_long) like '%$sw%' ";
     $sql .= 'order by bezug,calldate desc limit '.$_SESSION['listLimit'];
-	$rs = $db->getAll( $sql );
-	$used = Array();
-	if( $rs ) {	
+    $rs = $_SESSION['db']->getAll( $sql );
+    $used = Array();
+    if( $rs ) {    
         echo "<table id='treffer' class='tablesorter'>\n"; 
         echo "<thead><tr ><th>Datum</th><th>Grund</th><th>Name</th>\n<tbody>\n"; 
-		$i = 0;
-		foreach ( $rs as $row ) {
-			if ( $row["bezug"] > 0 and in_array($row["bezug"], $used) ) continue;
-			if ( $row["bezug"]==0 ) $used[]=$row["id"];
-			if ( strlen($row["cause"]) > 30 ) { $cause = substr($row["cause"], 0, 30).".."; }
-			else { $cause = $row["cause"]; };
-			if      ( $row["kname"] ) { $name = $row["kname"]; $src="C"; }
-			else if ( $row["lname"] ) { $name = $row["lname"]; $src="V";  }
-			else if ( $row["pname"] ) { $name = $row["pname"]; $src="CC"; }
-			else { $name = ""; $src='S'; }
-			echo "<tr onClick='showItem(".$row["id"].",\"$src\",".$row["caller_id"].");'>";
-			echo "<td>".db2date($row["calldate"])."&nbsp;</td><td> ".$cause."</td><td>";
-			echo "$name</td></tr>\n";
-			$i++;
-			if ($i>=$_SESSION['listLimit']) {
-				echo $_SESSION['listLimit']." von ".count($rs)." Treffern";
-				break;
-			}
-		}
-		echo "</tbody></table>\n<br>";
-	} else {
-		echo "Keine Treffer!";
-	}
+        $i = 0;
+        foreach ( $rs as $row ) {
+            if ( $row["bezug"] > 0 and in_array($row["bezug"], $used) ) continue;
+            if ( $row["bezug"]==0 ) $used[]=$row["id"];
+            if ( strlen($row["cause"]) > 30 ) { $cause = substr($row["cause"], 0, 30).".."; }
+            else { $cause = $row["cause"]; };
+            if      ( $row["kname"] ) { $name = $row["kname"]; $src="C"; }
+            else if ( $row["lname"] ) { $name = $row["lname"]; $src="V";  }
+            else if ( $row["pname"] ) { $name = $row["pname"]; $src="CC"; }
+            else { $name = ""; $src='S'; }
+            echo "<tr onClick='showItem(".$row["id"].",\"$src\",".$row["caller_id"].");'>";
+            echo "<td>".db2date($row["calldate"])."&nbsp;</td><td> ".$cause."</td><td>";
+            echo "$name</td></tr>\n";
+            $i++;
+            if ($i>=$_SESSION['listLimit']) {
+                echo $_SESSION['listLimit']." von ".count($rs)." Treffern";
+                break;
+            }
+        }
+        echo "</tbody></table>\n<br>";
+    } else {
+        echo "Keine Treffer!";
+    }
 } 
 else if (isset($_GET["adress"])) {
-	include("inc/FirmenLib.php");
-	include("inc/persLib.php");
-	include_once("inc/UserLib.php");
-	//ToDo Dialogtexte verbessern?
-	$msg='<div id="dialog" title="Kein Suchbegriff eingegeben">
-	            <p>Bitte geben Sie mindestens ein Zeichen ein.</p>
-	       </div>';
-	$viele='<div id="dialog" title="Zu viele Suchergebnisse">
-	            <p>Die Suche ergibt zu viele Resultate.</br> Bitte geben Sie mehr Zeichen ein.</p>
-	       </div>';
-	$keine='<div id="dialog" title="Nichts gefunden">
+    include("inc/FirmenLib.php");
+    include("inc/persLib.php");
+    include_once("inc/UserLib.php");
+    //ToDo Dialogtexte verbessern?
+    $msg   = '<div id="dialog" title="Kein Suchbegriff eingegeben">
+                <p>Bitte geben Sie mindestens ein Zeichen ein.</p>
+           </div>';
+    $viele = '<div id="dialog" title="Zu viele Suchergebnisse">
+                <p>Die Suche ergibt zu viele Resultate.</br> Bitte geben Sie mehr Zeichen ein.</p>
+           </div>';
+    $keine = '<div id="dialog" title="Nichts gefunden">
                 <p>Dieser Suchbegriff ergibt kein Resultat.</br>Bitte überprüfen Sie die Schreibweise!</p>
             </div>';
-	$found=false;
-	$suchwort=mkSuchwort($_GET["swort"]);
-	$anzahl=0;
-	$db->debug=0;
-
-	$rsE=getAllUser($suchwort);
-	if (chkAnzahl($rsE,$anzahl) && $_GET["swort"]) {
-		$rsV=getAllFirmen($suchwort,true,"V");	
-		if (chkAnzahl($rsV,$anzahl)) {
-			$rsC=getAllFirmen($suchwort,true,"C");
-			if (chkAnzahl($rsC,$anzahl)) {
-				$rsK=getAllPerson($suchwort);
-				if (!chkAnzahl($rsK,$anzahl)) {
-					$msg=$viele;
-				} else {
-					if ($anzahl===0) $msg=$keine;
-				} 
-			} else {
-				$msg=$viele;
-			}
-		} else {
-			$msg=$viele;
-		}
-	} 
-
-	if ($anzahl>0) {
+    $suchwort = mkSuchwort($_GET["swort"]);
+    $anzahl = 0;
+    #$rsE = $rsV = $rsC = $rsK = false;
+    if ( $_GET['swort'] != '') {
+        $rsC = getAllFirmen($suchwort,true,"C");
+        if ( $rsC ) $anzahl = count($rsC);
+        if ( $anzahl <= $_SESSION['listLimit'] ) {
+            $rsV = getAllFirmen($suchwort,true,"V");    
+            if ( $rsV ) $anzahl += count($rsV);
+            if ( $anzahl <= $_SESSION['listLimit'] ) {
+                $rsK = getAllPerson($suchwort);
+                if ( $rsK ) $anzahl += count($rsK);
+                if ( $anzahl <= $_SESSION['listLimit'] ) {
+                    $rsE = getAllUser($suchwort);
+                    if ( $rsE ) $anzahl += count($rsE);
+                    if ( $anzahl <= $_SESSION['listLimit'] ) {
+                        $msg = $viele;
+                    } else {
+                        if ($anzahl === 0) $msg = $keine;
+                    } 
+                } else {
+                    $msg = $viele;
+                }
+            } else {
+                $msg = $viele;
+            }
+        } else {
+                $msg = $viele;
+        }
+    }
+    if ($anzahl>0) {
         if ($anzahl==1 && $rsC) header("Location: firma1.php?Q=C&id=".$rsC[0]['id']); 
         if ($anzahl==1 && $rsV) header("Location: firma1.php?Q=V&id=".$rsV[0]['id']); 
         if ($anzahl==1 && $rsK) header("Location: kontakt.php?id=".$rsK[0]['id']); 
@@ -224,27 +227,27 @@ else if (isset($_GET["adress"])) {
         echo "<thead><tr ><th>KD-Nr</th><th>Name</th><th>Anschrift</th><th>Telefon</th><th></th></tr></thead>\n<tbody>\n"; 
         $i=0; 
         if ($rsC) foreach($rsC as $row) { 
-            echo "<tr class='bgcol".($i%2+1)."' onClick='showD(\"C\",".$row["id"].");'>". 
-                 "<td class=\"liste\">".$row["customernumber"]."</td><td class=\"liste\">".$row["name"]."</td>". 
-                 "<td class=\"liste\">".$row["city"].(($row["street"])?", ":"").$row["street"]."</td><td class=\"liste\">".$row["phone"]."</td><td class=\"liste\">K</td></tr>\n"; 
+            echo "<tr onClick='showD(\"C\",".$row["id"].");'>". 
+                 "<td>".$row["customernumber"]."</td><td class=\"liste\">".$row["name"]."</td>". 
+                 "<td>".$row["city"].(($row["street"])?", ":"").$row["street"]."</td><td class=\"liste\">".$row["phone"]."</td><td class=\"liste\">K</td></tr>\n"; 
             $i++; 
         }  
         if ($rsV) foreach($rsV as $row) { 
-            echo "<tr class='bgcol".($i%2+1)."' onClick='showD(\"V\",".$row["id"].");'>". 
-                 "<td class=\"liste\">".$row["vendornumber"]."</td><td class=\"liste\">".$row["name"]."</td>". 
-                 "<td class=\"liste\">".$row["city"].(($row["street"])?", ":"").$row["street"]."</td><td class=\"liste\">".$row["phone"]."</td><td class=\"liste\">L</td></tr>\n"; 
+            echo "<tr onClick='showD(\"V\",".$row["id"].");'>". 
+                 "<td>".$row["vendornumber"]."</td><td class=\"liste\">".$row["name"]."</td>". 
+                 "<td>".$row["city"].(($row["street"])?", ":"").$row["street"]."</td><td class=\"liste\">".$row["phone"]."</td><td class=\"liste\">L</td></tr>\n"; 
             $i++; 
         } 
         if ($rsK) foreach($rsK as $row) { 
-            echo "<tr class='bgcol".($i%2+1)."' onClick='showD(\"K\",".$row["cp_id"].");'>". 
-                 "<td class=\"liste\">".$row["cp_id"]."</td><td class=\"liste\">".$row["cp_name"].", ".$row["cp_givenname"]."</td>". 
-                 "<td class=\"liste\">".$row["cp_city"].(($row["cp_street"])?", ":"").$row["cp_street"]."</td><td class=\"liste\">".$row["cp_phone1"]."</td><td class=\"liste\">P</td></tr>\n"; 
+            echo "<tr onClick='showD(\"K\",".$row["cp_id"].");'>". 
+                 "<td>".$row["cp_id"]."</td><td class=\"liste\">".$row["cp_name"].", ".$row["cp_givenname"]."</td>". 
+                 "<td>".$row["cp_city"].(($row["cp_street"])?", ":"").$row["cp_street"]."</td><td class=\"liste\">".$row["cp_phone1"]."</td><td class=\"liste\">P</td></tr>\n"; 
             $i++; 
         } 
         if ($rsE) foreach($rsE as $row) { 
-            echo "<tr class='bgcol".($i%2+1)."' onClick='showD(\"E\",".$row["id"].");'>". 
-                 "<td class=\"liste\">".$row["id"]."</td><td class=\"liste\">".$row["name"]."</td>". 
-                 "<td class=\"liste\">".$row["addr2"].(($row["addr1"])?", ":"").$row["addr1"]."</td><td class=\"liste\">".$row["workphone"]."</td><td class=\"liste\">U</td></tr>\n"; 
+            echo "<tr onClick='showD(\"E\",".$row["id"].");'>". 
+                 "<td>".$row["id"]."</td><td class=\"liste\">".$row["name"]."</td>". 
+                 "<td>".$row["addr2"].(($row["addr1"])?", ":"").$row["addr1"]."</td><td class=\"liste\">".$row["workphone"]."</td><td class=\"liste\">U</td></tr>\n"; 
             $i++; 
         } 
         echo "</tbody></table>\n"; 
@@ -254,29 +257,23 @@ else if (isset($_GET["adress"])) {
                 <img src="'.$_SESSION['baseurl'].'crm/jquery-ui/plugin/Table/addons/pager/icons/first.png" class="first"/>
                 <img src="'.$_SESSION['baseurl'].'crm/jquery-ui/plugin/Table/addons/pager/icons/prev.png" class="prev"/>
                 <img src="'.$_SESSION['baseurl'].'crm/jquery-ui/plugin/Table/addons/pager/icons/next.png" class="next"/>
-                <img src="'.$_SESSION['baseurl'].'crm/jquery-ui/plugin/Table/addons/pager/icons/last.png" class="last"/>
+                <img src="'.$_SESSION['baseurl'].'crm/jquery-ui/plugin/Table/addons/pager/icons/last.png" class="last"/> '.$anzahl.'
                 <select class="pagesize" id="pagesize">
                     <option value="10">10</option>
                     <option value="20" selected>20</option>
                     <option value="30">30</option>
                     <option value="40">40</option>
                 </select>
-            </form>
+            </form> 
         </span>';
    
-    } 
-    else { 
+    } else { 
         echo $msg; 
     }; 
 } //END ELSEIF adress
     if (isset($_GET['kontakt']) || isset($_GET['adress'])) {
 ?>
-
-  
-        <br>
-
-
-
+       <br>
 <?php } 
     echo $menu['end_content'];
     ob_end_flush(); 

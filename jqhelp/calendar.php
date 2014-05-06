2038286b804a3945e42abd41bae44d5520779f7e
Ronny Kumke <ronny@lxcars.de>
2014-05-06 07:01:57 +0200

    allDay eingeführt
    ToDo: invalid endDate beim schieben

--- jqhelp/calendar.php
+++ jqhelp/calendar.php
@@ -7,32 +7,39 @@
     $end   = $_POST['end'];
     $desc  = $_POST['desc'];
     $id    = $_POST['id'];
+    $allDay = $_POST['allDay'];
     //$url = $_POST['url'];
     switch( $task ){
         case "newEvent":
-            $sql="INSERT INTO termine (start, stop, cause, c_cause) VALUES ( '$start'::TIMESTAMP, '$end'::TIMESTAMP,'$title','$desc' )";
+            $sql="INSERT INTO termine (start, stop, cause, c_cause, allDay) VALUES ( '$start'::TIMESTAMP, '$end'::TIMESTAMP,'$title','$desc', $allDay )";
             $rc=$_SESSION['db']->query($sql); 
             $sql = "SELECT MAX(id) FROM termine";
             $rs = $_SESSION['db']->getOne($sql);
             echo $rs['max'];  
         break;
+        case "updateEvent":
+            $sql="UPDATE termine SET cause = '$title', start = '$start'::TIMESTAMP, stop = '$end'::TIMESTAMP, c_cause = '$desc', allday = $allDay WHERE id = $id";
+            $rc=$_SESSION['db']->query($sql);   
+        break;
         case "updateTimestamp":
             $sql="UPDATE termine SET  start = '$start'::TIMESTAMP, stop = '$end'::TIMESTAMP WHERE id = $id";
             $rc=$_SESSION['db']->query($sql);   
         break;
-        case "updateEvent":
-            $sql="UPDATE termine SET cause = '$title', start = '$start'::TIMESTAMP, stop = '$end'::TIMESTAMP, c_cause = '$desc' WHERE id = $id";
-            $rc=$_SESSION['db']->query($sql);   
-        break;
-        case "getEvents":
-            $sql="SELECT cause AS title, start, stop AS end, c_cause AS desc, id FROM termine";
-            $rs=$_SESSION['db']->getAll($sql); 
-            echo json_encode($rs);  
-        break; 
         case "deleteEvent":
             $sql="DELETE FROM termine WHERE id = $id";
             $rc=$_SESSION['db']->query($sql);   
         break;
+        case "getEvents":
+            $sql="SELECT cause AS title, start, stop AS end, c_cause AS desc, id, allday FROM termine";
+            $rs=$_SESSION['db']->getAll($sql);
+            foreach( $rs as $key => $value ){
+                $rs[$key]['allDay'] = $rs[$key]['allday'] == 't' ? true : false;
+                unset( $rs[$key]['allday'] );
+            }
+            //print_r( $rs ); 
+            echo json_encode($rs);  
+        break; 
+       
      }    
    
     return true;

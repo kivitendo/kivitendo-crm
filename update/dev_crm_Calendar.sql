28e39b68a0c2025fecf14e0717bcfd4b1aa59735
Ronny Kumke <ronny@lxcars.de>
2014-06-03 06:37:28 +0200

    Calendar: Aufgabe implementiert

--- update/dev_crm_Calendar.sql
+++ update/dev_crm_Calendar.sql
@@ -6,4 +6,5 @@ CREATE SEQUENCE termine_id_seq;
 ALTER TABLE termine ALTER id SET DEFAULT NEXTVAL('termine_id_seq');
 ALTER TABLE termine ADD COLUMN allday bool;
 ALTER TABLE termine ADD COLUMN prio char;
+ALTER TABLE termine ADD COLUMN job bool;
 

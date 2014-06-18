1dcbe242307a7bb7a42d77c59c69bbb5b5cee9c8
Ronny Kumke <ronny@lxcars.de>
2014-06-18 05:04:11 +0200

    Calendar: Location implementiert

--- jqhelp/calendar.php
+++ jqhelp/calendar.php
@@ -16,17 +16,18 @@
     $job        = $_POST['job'];
     $color      = $_POST['color'];
     $done       = $_POST['done'];
+    $location    = $_POST['location'];
     //$url = $_POST['url'];
     switch( $task ){
         case "newEvent":
-            $sql="INSERT INTO termine (start, stop, cause, c_cause, allDay, prio, job, color, done) VALUES ( '$start'::TIMESTAMP, '$end'::TIMESTAMP,'$title','$desc', $allDay, $prio, '$job', '$color', '$done' )";
+            $sql="INSERT INTO termine (start, stop, cause, c_cause, allDay, prio, job, color, done, location ) VALUES ( '$start'::TIMESTAMP, '$end'::TIMESTAMP,'$title','$desc', $allDay, $prio, '$job', '$color', '$done', '$location' )";
             $rc=$_SESSION['db']->query($sql); 
             //$sql = "SELECT MAX(id) FROM termine";//!!!!!!!!!!!!!!!!!!!!
             //$rs = $_SESSION['db']->getOne($sql);
             //echo $rs['max'];  
         break;
         case "updateEvent":
-            $sql="UPDATE termine SET cause = '$title', start = '$start'::TIMESTAMP, stop = '$end'::TIMESTAMP, c_cause = '$desc', allday = $allDay, uid = '$uid', prio = '$prio', job = '$job', color = '$color', done = '$done' WHERE id = $id";
+            $sql="UPDATE termine SET cause = '$title', start = '$start'::TIMESTAMP, stop = '$end'::TIMESTAMP, c_cause = '$desc', allday = $allDay, uid = '$uid', prio = '$prio', job = '$job', color = '$color', done = '$done', location = '$location' WHERE id = $id";
             $rc=$_SESSION['db']->query($sql);   
         break;
         case "updateTimestamp":
@@ -38,7 +39,7 @@
             $rc=$_SESSION['db']->query($sql);   
         break;
         case "getEvents":
-            $sql="SELECT cause AS title, start, stop AS end, c_cause AS desc, id, allday, uid, prio, job, color, done FROM termine WHERE start <= '$endGet' AND stop >= '$startGet'";
+            $sql="SELECT cause AS title, start, stop AS end, c_cause AS desc, id, allday, uid, prio, job, color, done, location FROM termine WHERE start <= '$endGet' AND stop >= '$startGet'";
             $rs=$_SESSION['db']->getAll($sql);
             foreach( $rs as $key => $value ){
                 $rs[$key]['allDay'] = $rs[$key]['allday'] == 't' ? true : false;

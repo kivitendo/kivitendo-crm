5adf247582b4c3527f8ce2107f1789fd52d4071e
Ronny Kumke <ronny@lxcars.de>
2014-06-19 10:06:05 +0200

    Calendar: Location mit Doppelklick für weitere Infos, Wiederholungen erdacht

--- update/dev_crm_Calendar.sql
+++ update/dev_crm_Calendar.sql
@@ -10,4 +10,5 @@ ALTER TABLE termine ADD COLUMN job bool;
 ALTER TABLE termine ADD COLUMN color char(7);
 ALTER TABLE termine ADD COLUMN done bool;
 ALTER TABLE termine ADD COLUMN cust_vend_pers char(32);
+/* Neue Tabelle termin_repeat erstellen. Inhalt: id (fremdschlüssel), anz-wdhl, end-date::TIMESTAMP 
 

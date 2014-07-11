167fdac80a18818e37725d2240de5777a7fe8252
Ronny Kumke <ronny@lxcars.de>
2014-07-11 22:14:32 +0200

    Calendar: angefangen Repeats zu implementieren !!! unfertig !!!

--- jqhelp/calendar.php
+++ jqhelp/calendar.php
@@ -14,7 +14,7 @@
     //$url = $_POST['url'];
     switch( $task ){
         case "newEvent":
-            $sql="INSERT INTO events (start, stop, title, description, allDay, uid, visibility, category, prio, job, color, done, location, cust_vend_pers ) VALUES ( '$start'::TIMESTAMP, '$end'::TIMESTAMP,'$title','$desc', $allDay, $uid, $visibility, $category, $prio, '$job', '$color', '$done', '$location', '$cust_vend_pers' )";
+            $sql="INSERT INTO events (start, stop, title, description, allDay, uid, visibility, category, prio, job, color, done, location, cust_vend_pers, repeat, repeat_factor, repeat_quantity, repeat_end ) VALUES ( '$start'::TIMESTAMP, '$end'::TIMESTAMP,'$title','$desc', $allDay, $uid, $visibility, $category, $prio, '$job', '$color', '$done', '$location', '$cust_vend_pers', '$repeat', '$repeat_factor', '$repeat_quantity', '$repeat_end'::TIMESTAMP )";
             $rc=$_SESSION['db']->query($sql); 
             echo "SQL: ".$sql;
             //$sql = "SELECT MAX(id) FROM events";//!!!!!!!!!!!!!!!!!!!!
@@ -22,7 +22,7 @@
             //echo $rs['max'];  
         break;
         case "updateEvent":
-            $sql="UPDATE events SET title = '$title', start = '$start'::TIMESTAMP, stop = '$end'::TIMESTAMP, description = '$desc', allday = $allDay, uid = '$uid', visibility = '$visibility', category = '$category', prio = '$prio', job = '$job', color = '$color', done = '$done', location = '$location', cust_vend_pers = '$cust_vend_pers' WHERE id = $id";
+            $sql="UPDATE events SET title = '$title', start = '$start'::TIMESTAMP, stop = '$end'::TIMESTAMP, description = '$desc', allday = $allDay, uid = '$uid', visibility = '$visibility', category = '$category', prio = '$prio', job = '$job', color = '$color', done = '$done', location = '$location', cust_vend_pers = '$cust_vend_pers', repeat = '$repeat', repeat_factor = '$repeat_factor', repeat_quantity = '$repeat_quantity', repeat_end = '$repeat_end'::TIMESTAMP  WHERE id = $id";
             $rc=$_SESSION['db']->query($sql);   
         break;
         case "updateTimestamp":
@@ -34,7 +34,7 @@
             $rc=$_SESSION['db']->query($sql);   
         break;
         case "getEvents":
-            $sql="SELECT title, start, stop AS end, description AS desc, id, allday, uid, visibility, category, prio, job, color, done, location, cust_vend_pers FROM events WHERE start <= '$endGet' AND stop >= '$startGet' AND CASE WHEN visibility = 0 THEN uid = $myuid ELSE TRUE END";
+            $sql="SELECT title, start, stop AS end, description AS desc, id, allday, uid, visibility, category, prio, job, color, done, location, cust_vend_pers, repeat FROM events WHERE start <= '$endGet' AND stop >= '$startGet' AND CASE WHEN visibility = 0 THEN uid = $myuid ELSE TRUE END";
             //echo $sql;            
             $rs=$_SESSION['db']->getAll($sql);
             foreach( $rs as $key => $value ){

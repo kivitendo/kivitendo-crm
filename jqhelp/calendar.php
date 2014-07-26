672a469b6635e6fc0eed81443bff564b0a6381c8
Ronny Kumke <ronny@lxcars.de>
2014-07-26 20:37:25 +0200

    Calendar: DB-Abfragen geben Json zurück

--- jqhelp/calendar.php
+++ jqhelp/calendar.php
@@ -31,35 +31,20 @@
             $rc=$_SESSION['db']->query($sql);   
         break;
         case "getEvents":
-            //$sql="SELECT title, start, stop AS end, description AS desc, id, allday, uid, visibility, category, prio, job, color, done, location, cust_vend_pers, repeat, repeat_factor, repeat_quantity, repeat_end FROM events WHERE start <= '$endGet' AND stop >= '$startGet' AND CASE WHEN visibility = 0 THEN uid = $myuid ELSE TRUE END";
-            $sql = "select *, lower(tsrange) AS start, upper(tsrange) AS end  FROM (select id, title, repeat, repeat_factor, repeat_quantity, repeat_end, description, location, uid, visibility,  prio, category, \"allDay\", color, job, done, job_planned_end, cust_vend_pers, row_number - 1 AS repeat_num, tsrange(lower(duration) + (row_number - 1)::INT * (repeat_factor||repeat)::interval, upper(duration) + (row_number - 1)::INT * (repeat_factor||repeat)::interval) from (select t.*, row_number() over (partition by id) from events t cross join lateral (select generate_series(0, t.repeat_quantity ) i) x) foo) alle_termine where '[$startGet, $endGet)'::tsrange @> tsrange AND CASE WHEN visibility = 0 THEN uid = $myuid ELSE TRUE END";
-          // " // ToDo: Visibility für Gruppen implementieren.            
-            //echo $sql;            
-            $rs=$_SESSION['db']->getAll($sql);
-            foreach( $rs as $key => $value ){
-                $rs[$key]['allDay'] = $rs[$key]['allday'] == 't' ? true : false;
-                unset( $rs[$key]['allday'] );
-                $rs[$key]['done'] = $rs[$key]['done'] == 't' ? true : false;
-            }
-            //print_r( $rs ); 
-            echo json_encode( $rs );  
+            $sql = "SELECT json_agg(json_event) FROM ( select *, lower( tsrange ) AS start, upper( tsrange ) AS end  FROM ( select id, title, repeat, repeat_factor, repeat_quantity, repeat_end, description, location, uid, visibility,  prio, category, \"allDay\", color, job, done, job_planned_end, cust_vend_pers, row_number - 1 AS repeat_num, tsrange(lower(duration) + (row_number - 1)::INT * (repeat_factor||repeat)::interval, upper(duration) + (row_number - 1)::INT * (repeat_factor||repeat)::interval) from (select t.*, row_number() over (partition by id) from events t cross join lateral (select generate_series(0, t.repeat_quantity ) i) x) foo) alle_termine where '[$startGet, $endGet)'::tsrange @> tsrange AND CASE WHEN visibility = 0 THEN uid = $myuid ELSE TRUE END ) json_event";
+            // " // ToDo: Visibility für Gruppen implementieren.            
+            $rs = $_SESSION['db']->getOne( $sql );
+            echo $rs['json_agg'];   
         break; 
         case "getUsers":
-            $sql="SELECT id AS value, name AS text FROM employee WHERE deleted = FALSE"; //login
-            $rs=$_SESSION['db']->getAll( $sql );
-            //print_r( $rs ); 
-            echo json_encode( $rs ) ;  
+            $sql="SELECT json_agg( json_employee ) FROM (SELECT id AS value, name AS text FROM employee WHERE deleted = FALSE ) json_employee"; //login
+            $rs=$_SESSION['db']->getOne( $sql );    
+            echo $rs['json_agg'];  
         break;
         case "getCategory":
-            $sql="SELECT id AS value, label AS text FROM event_category ORDER BY id"; 
-            $rs=$_SESSION['db']->getAll( $sql );
-            //print_r( $rs ); 
-            echo json_encode( $rs ) ;  
+            $sql="SELECT json_agg( json_category ) FROM (SELECT id AS value, label AS text FROM event_category ORDER BY id ) json_category"; 
+            $rs=$_SESSION['db']->getOne( $sql ); 
+            echo $rs['json_agg'];  
         break;
-        
-       
      }    
-   
-    return true;
-    
 ?>	

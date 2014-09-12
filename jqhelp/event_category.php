8062d0ea8dc65223d7f0f984fd7e481ffde60ad1
Ronny Kumke <ronny@lxcars.de>
2014-09-12 06:40:29 +0200

    EventCategory: Update mit "data-modifying common table expression" realisiert
    ToDo: remove() durch attr() ersetzen

--- jqhelp/event_category.php
+++ jqhelp/event_category.php
@@ -1,10 +1,12 @@
 <?php
     require_once("../inc/stdLib.php"); 
     require_once("../inc/crmLib.php");  
-    $task  = $_POST['task'] ? $_POST['task'] : $_GET['task'];
+    $task  = array_shift( $_POST );
     $newCat = $_POST['newCat'];
     $newColor = $_POST['newColor'];
-    //echo "Task: ".$task;
+   // echo "Task: ".$task;
+  
+    
     switch( $task ){
         case "newCategory":
             $sql="INSERT INTO event_category ( label, color, cat_order ) VALUES ( '$newCat', '$newColor', ( SELECT max( cat_order ) + 1 AS cat_order FROM event_category) )";
@@ -12,16 +14,25 @@
         break;
         
         case "getCategories":
-            $sql = "SELECT json_agg( json_category ) FROM ( SELECT id, label, color FROM event_category ORDER BY cat_order DESC ) AS json_category ;";
+            $sql = "SELECT json_agg( json_category ) FROM ( SELECT id, label, TRIM( color ) AS color FROM event_category ORDER BY cat_order DESC ) AS json_category ;";
             //echo $sql;            
             $rs = $_SESSION['db']->getOne( $sql );
             echo $rs['json_agg'];   
         break; 
         case "updateCategories":
-            $sql = "     ";
-            //echo $sql;            
+            $data = array_shift( $_POST );
+            //print_r( $data );
+            $sql = "WITH new_data (id, label, color, cat_order) AS ( VALUES ";
+            foreach( $data as $key => $value ){
+                $order = ( int ) ( $key / 2 );
+                if( $key % 2 ) $sql .= ", '".$value['value']."', ".$order." )";//\r\n
+                else $sql .= ($key ? ',' :'' )."( ".substr($value['name'], 4).", '".$value['value']."'";
+                
+            }
+            $sql .= " ) UPDATE event_category SET label = d.label, color = d.color, cat_order = d.cat_order FROM new_data d WHERE d.id = event_category.id";
+            //echo $sql;          
             $rs = $_SESSION['db']->getOne( $sql );
-            echo $rs['json_agg'];   
+              
         break;
      }
  ?>

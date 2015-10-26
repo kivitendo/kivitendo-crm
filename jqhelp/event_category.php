f6a28512ebba30d77f7b3c0fea73c10035bae85e
Holger Lindemann <hli@lx-system.de>
2015-10-26 14:52:05 +0100

    Auch ohne JSon-Fähigkeit der DB

--- jqhelp/event_category.php
+++ jqhelp/event_category.php
@@ -5,8 +5,6 @@
     $newCat   = $_POST['newCat'];
     $newColor = $_POST['newColor'];
     $delCat   = $_POST['delCat'];   
-   // echo "Task: ".$task;
-  
     
     switch( $task ){
         case "newCategory":
@@ -14,29 +12,30 @@
             $rc=$_SESSION['db']->query($sql); 
         break;
         case "getCategories":
-            $sql = "SELECT json_agg( json_category ) FROM ( SELECT id, label, TRIM( color ) AS color FROM event_category ORDER BY cat_order DESC ) AS json_category ;";
-            //echo $sql;            
-            $rs = $_SESSION['db']->getOne( $sql );
-            echo $rs['json_agg'];   
+            if ( $_SESSION['db']->JVer ) {
+                $sql = "SELECT json_agg( json_category ) FROM ( SELECT id, label, TRIM( color ) AS color FROM event_category ORDER BY cat_order DESC ) AS json_category ;";
+                $rs = $_SESSION['db']->getOne( $sql );
+                echo $rs['json_agg'];   
+            } else {
+                $sql = "SELECT id , label, TRIM( color ) AS color FROM event_category ORDER BY cat_order DESC";
+                $rs = $_SESSION['db']->getJson( $sql );
+                echo $rs;  
+            }
         break; 
         case "updateCategories":
-            $data = array_shift( $_POST );
-            //print_r( $data );
-            $sql = "WITH new_data (id, label, color, cat_order) AS ( VALUES ";
-            foreach( $data as $key => $value ){
-                $order = ( int ) ( $key / 2 );
-                if( $key % 2 ) $sql .= ", '".$value['value']."', ".$order." )";//\r\n
-                else $sql .= ($key ? ',' :'' )."( ".substr($value['name'], 4).", '".$value['value']."'";
-                
-            }
-            $sql .= " ) UPDATE event_category SET label = d.label, color = d.color, cat_order = d.cat_order FROM new_data d WHERE d.id = event_category.id";
-            //echo $sql;          
-            $rs = $_SESSION['db']->getOne( $sql );
-              
+            if ( $_POST['updateData']) { 
+                $sql = "WITH new_data (id, label, color, cat_order) AS ( VALUES ";
+                foreach( $_POST['updateData'] as $key => $value ){
+                    $order = ( int ) ( $key / 2 );
+                    if( $key % 2 ) $sql .= ", '".$value['value']."', ".$order." )";//\r\n
+                    else $sql .= ($key ? ',' :'' )."( ".substr($value['name'], 4).", '".$value['value']."'";        
+                }
+                $sql .= " ) UPDATE event_category SET label = d.label, color = d.color, cat_order = d.cat_order FROM new_data d WHERE d.id = event_category.id";
+                $rs = $_SESSION['db']->getOne( $sql );
+            }      
         break;
         case "deleteCategory":
             $sql="DELETE FROM event_category WHERE id = $delCat";
-            //echo $sql;            
             $rc=$_SESSION['db']->query($sql); 
         break;
      }

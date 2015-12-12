d53bc816548fcbac7a88a00c8474c00290e415c5
Ronny Kumke <ronny@lxcars.de>
2015-12-12 16:02:02 +0100

    Revert "vergessenes JQ-Help-File geadded"
    This reverts commit 1528f0b957efd3cf9974e569593c228a51043b65.

--- jqhelp/getJsInline.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // Behelfsmethode da die Template-Engine das Inline-JavaScript (kivi.myconfig) zerstört
-    include "../inc/stdLib.php";    
-    echo $_SESSION['menu']['javascripts_inline'];
-    //echo 'test';
-?>

f98a27d27991d630f6730a19de90e0d3a7d72377
Kevin Hanke <kevin@inter-data.de>
2016-01-27 11:17:39 +0100

    Weiterarbeitung an Tools in ERP - TODO POSTIT

--- js/ERPplugins.js
+++ js/ERPplugins.js
@@ -1,10 +1,19 @@
-document.write("<link rel='stylesheet' type='text/css' href='crm/jquery-plugins/jquery-calculator/jquery.calculator.css'>");
 document.write("<script type='text/javascript' src='crm/jquery-plugins/jquery-calculator/jquery.plugin.js'></script>");
 document.write("<script type='text/javascript' src='crm/jquery-plugins/jquery-calculator/jquery.calculator.js'></script>");
 document.write("<script type='text/javascript' src='crm/jquery-plugins/jquery-calculator/jquery.calculator-de.js'></script>");
-document.write("<link rel='stylesheet' type='text/css' href='crm/jquery-plugins/fancybox/source/jquery.fancybox.css'>");
 document.write("<script type='text/javascript' src='crm/jquery-plugins/fancybox/source/jquery.fancybox.pack.js'></script>");
+//document.write("<script type='text/javascript' src='crm/js/jquery.postitall.js'></script>");
+//document.write("<script type='text/javascript' src='crm/nodejs/node_modules/trumbowyg/dist/trumbowyg.min.js'></script>");
+//document.write("<script type='text/javascript' src='crm/nodejs/node_modules/jquery-minicolors/jquery.minicolors.min.js");
+//document.write("<script type='text/javascript' src='crm/js/jquery.postitall.ajax.js");
 document.write("<script type='text/javascript' src='crm/js/tools.js'></script>");
+
+document.write("<link rel='stylesheet' type='text/css' href='crm/jquery-plugins/jquery-calculator/jquery.calculator.css'>");
+document.write("<link rel='stylesheet' type='text/css' href='crm/nodejs/node_modules/trumbowyg/dist/ui/trumbowyg.css'>");
+
+document.write("<link rel='stylesheet' type='text/css' href='crm/nodejs/node_modules/postitall/dist/jquery.postitall.css'>");
+document.write("<link rel='stylesheet' type='text/css' href='crm/jquery-plugins/fancybox/source/jquery.fancybox.css'>");
+
 
 document.write("<div class='fancybox' data-fancybox-type='iframe' href=''></div>");
 
@@ -33,6 +42,8 @@ $(document).ready(function() {
     });
       
     $("h1:first").addClass( "ui-state-highlight ui-corner-all tools" );
-    //$("div.listtop").addClass( "ui-state-highlight ui-corner-all tools" );
-
+    $( "h1:first" ).css({
+        marginTop: "20px",
+        height: "20px"
+    });
 });

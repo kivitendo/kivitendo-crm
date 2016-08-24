17bbfe603cc40cb36ae954ee729e87c968a447e7
Bernhard Strunck <bernhard@inter-data.de>
2016-08-24 16:35:49 +0200

    Dialog zur Datenanzeige in example2 hinzugefügt

--- ajax/contact.php
+++ ajax/contact.php
@@ -3,28 +3,20 @@
 require_once __DIR__.'/../inc/ajax2function.php';
 
 function newContact( $data ){
-    writeLog($data);
+    //writeLog($data);
     $data = ( array ) json_decode( $data );
-    writeLog($data);
+    //writeLog($data);
     //wenn id == 0 neuen Datensatz, sonst Update     
-    if ($data['id'] == 0) {
-        $rs = $GLOBALS['dbh']->insert( 'contact_events', array( 'cause', 'calldate','caller_id', 'contact_reference', 'employee', 'cause_long', 'type_of_contact', 'inout' ),
+    $data['id'] ? $rs = $GLOBALS['dbh']->update( 'contact_events', array( 'cause', 'calldate','caller_id', 'contact_reference', 'employee', 'cause_long', 'type_of_contact', 'inout' ),
+            array( $data['cause'], $data['calldate'], $data['caller_id'], 0, $data['employee'], $data['cause_long'], $data['type_of_contact'], $data['inout'] ), "id =" .$data['id'])
+        :   $rs = $GLOBALS['dbh']->insert( 'contact_events', array( 'cause', 'calldate','caller_id', 'contact_reference', 'employee', 'cause_long', 'type_of_contact', 'inout' ),
             array( $data['cause'], $data['calldate'], $data['caller_id'], 0, $data['employee'], $data['cause_long'], $data['type_of_contact'], $data['inout'] ) );
-    } else {
-         $rs = $GLOBALS['dbh']->update( 'contact_events', array( 'cause', 'calldate','caller_id', 'contact_reference', 'employee', 'cause_long', 'type_of_contact', 'inout' ),
-            array( $data['cause'], $data['calldate'], $data['caller_id'], 0, $data['employee'], $data['cause_long'], $data['type_of_contact'], $data['inout'] ), "id =" .$data['id']);
-    }
-    writeLog( $rs );
     echo 1;
 }
-
-
 
 function getData(){
     //alle Datensätze bereitstellen
     $rs = $GLOBALS[ 'dbh' ]->getAll( 'SELECT * FROM contact_events', true );
-//    $rs = $GLOBALS[ 'dbh' ]->getAll( 'SELECT * contact_events', true );
-    writeLog( $rs );
     echo $rs;
 
 }

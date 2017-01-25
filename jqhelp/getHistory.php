323f24d5c349dc7a487acd4b5ff123a4ecf55249
Ronny Kumke <ronny@lxcars.de>
2017-01-25 07:49:49 +0100

    jqhelp getHistory entfernt

--- jqhelp/getHistory.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("../inc/stdLib.php");
-$history_data = accessHistory();
-$liste = '';
-if ( $history_data ) foreach ( $history_data as $key => $value ) {
-    $liste .= '<li id="'.$value[2].$value[0].'"><a href="javascript:void(0);">'.$value[1].'</a></li>';
-}
-echo $liste;
-?>

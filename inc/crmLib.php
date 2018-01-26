9574a0b51501a216fbde496dae1eba4f721f9d2e
Ronny Zimmermann <ronny@inter-data.de>
2018-01-26 12:23:33 +0100

    Firma1: Wenn LxCars installiert ist dessen Instructions mit anzeigen.

--- inc/crmLib.php
+++ inc/crmLib.php
@@ -3264,8 +3264,15 @@ function getIOQ($fid,$Q,$type,$close){
             $sql.= "invnumber as number, $ar_ap.id FROM $ar_ap LEFT JOIN invoice  ON $ar_ap.id=trans_id LEFT JOIN currencies C on currency_id=C.id  WHERE $cust_vend = $fid ORDER BY $ar_ap.id DESC, invoice.id";
             break;
         case "ord": //Aufträge
-            $sql = "SELECT DISTINCT ON (oe.id) to_char(oe.transdate, 'DD.MM.YYYY') as date, description, COALESCE(ROUND(amount,2))||' '||COALESCE(C.name) as amount, ";
-            $sql.= "oe.ordnumber as number, oe.id FROM oe LEFT JOIN orderitems ON oe.id=trans_id LEFT JOIN currencies C on currency_id=C.id WHERE quotation = FALSE AND $cust_vend = $fid ORDER BY oe.id DESC, orderitems.id";
+            if( file_exists( __DIR__.'/../lxcars' ) ){
+                $sql = "SELECT DISTINCT ON (oe.id) to_char(oe.transdate, 'DD.MM.YYYY') as date, COALESCE( instructions.description,'orderitems.description' ) AS description, COALESCE(ROUND(amount,2))||' '||COALESCE(C.name) as amount, ";
+                $sql.= "oe.ordnumber as number, oe.id FROM oe LEFT JOIN orderitems ON oe.id = orderitems.trans_id LEFT JOIN instructions ON oe.id = instructions.trans_id LEFT JOIN currencies C on currency_id=C.id WHERE quotation = FALSE AND $cust_vend = $fid  ORDER BY oe.id DESC, orderitems.id";
+            }
+            else{
+                $sql = "SELECT DISTINCT ON (oe.id) to_char(oe.transdate, 'DD.MM.YYYY') as date, description, COALESCE(ROUND(amount,2))||' '||COALESCE(C.name) as amount, ";
+                $sql.= "oe.ordnumber as number, oe.id FROM oe LEFT JOIN orderitems ON oe.id=trans_id LEFT JOIN currencies C on currency_id=C.id WHERE quotation = FALSE AND $cust_vend = $fid ORDER BY oe.id DESC, orderitems.id";
+                //writeLog( $sql );
+            }
             break;
         case "quo": //Angebote
             $sql = "SELECT DISTINCT ON (oe.id) to_char(oe.transdate, 'DD.MM.YYYY') as date, description, COALESCE(ROUND(amount,2))||' '||COALESCE(C.name) as amount, ";

94e6a25976968f0789ee3e1c5f15fff6e4e590e3
Thomas Richter <thomas@inter-data.de>
2018-02-17 16:46:55 +0100

    Quellcode fachgerecht formatiert...

--- ajax/firmen3.php
+++ ajax/firmen3.php
@@ -5,7 +5,7 @@ function getData( $data ){
     $table = $data['type'] == 'C' ? 'customer' : 'vendor';
     $term = trim( substr( $data['term'], strrpos( $data['term'], ' ') ) );
     $sql = "SELECT name AS value, '".$data['type']."' AS type, id  FROM $table WHERE name ILIKE '".$term."%' LIMIT 10";
-    if( strlen( $term ) > 2 ) echo $GLOBALS[ 'dbh' ]->getAll( $sql, true );
+    if( strlen( $term ) > 2 ) echo $GLOBALS['dbh']->getAll( $sql, true );
 }
 
 ?>

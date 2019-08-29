e7db244cb0ee8ccf4a93278095bca543506e225a
Ronny Zimmermann <ronny@inter-data.de>
2019-08-29 21:44:54 +0200

    Event Category: auf ajax2function ungestellt
    ToDo: Übersetzung, Colorpicker reparieren

--- ajax/event_category.php
+++ ajax/event_category.php
@@ -1,37 +1,32 @@
 <?php
-    //ToDo: use ajax2function, impement updateAll in phpDataObjects, translate eventcategory.phtml!!!!
-    require_once("../inc/stdLib.php");
-    require_once("../inc/crmLib.php");
-    $task     = array_shift( $_POST );
-    $newCat   = $_POST['newCat'];
-    $newColor = $_POST['newColor'];
-    $delCat   = $_POST['delCat'];
+    require_once __DIR__.'/../inc/ajax2function.php';
 
-    switch( $task ){
-        case "newCategory":
-            $sql="INSERT INTO event_category ( label, color, cat_order ) VALUES ( '$newCat', '$newColor', ( SELECT max( cat_order ) + 1 AS cat_order FROM event_category) )";
-            $rc=$GLOBALS['dbh']->query($sql);
-        break;
-        case "getCategories":
-            $sql = "SELECT id , label, TRIM( color ) AS color FROM event_category ORDER BY cat_order DESC";
-            $rs = $GLOBALS['dbh']->getAll( $sql, $json = TRUE );
-            echo $rs;
-        break;
-        case "updateCategories":
-            if ( $_POST['updateData']) {
-                $sql = "WITH new_data (id, label, color, cat_order) AS ( VALUES ";
-                foreach( $_POST['updateData'] as $key => $value ){
-                    $order = ( int ) ( $key / 2 );
-                    if( $key % 2 ) $sql .= ", '".$value['value']."', ".$order." )";//\r\n
-                    else $sql .= ($key ? ',' :'' )."( ".substr($value['name'], 4).", '".$value['value']."'";
-                }
-                $sql .= " ) UPDATE event_category SET label = d.label, color = d.color, cat_order = d.cat_order FROM new_data d WHERE d.id = event_category.id";
-                $rs = $GLOBALS['dbh']->getOne( $sql );
-            }
-        break;
-        case "deleteCategory":
-            $sql="DELETE FROM event_category WHERE id = $delCat";
-            $rc=$GLOBALS['dbh']->query($sql);
-        break;
+    function newCategory( $data ){
+        $sql="INSERT INTO event_category ( label, color, cat_order ) VALUES ( '".$data['newCat']."', '".$data['newColor']."', ( SELECT max( cat_order ) + 1 AS cat_order FROM event_category) )";
+        writeLog( $sql );
+        $rc=$GLOBALS['dbh']->query($sql);
+    }
+
+    function getCategories(){
+        $sql = "SELECT id , label, TRIM( color ) AS color FROM event_category ORDER BY cat_order DESC";
+        echo $GLOBALS['dbh']->getAll( $sql, $json = TRUE );
      }
+
+     function updateCategories( $data ){
+        writeLog( $data );
+        $sql = "WITH new_data (id, label, color, cat_order) AS ( VALUES ";
+        foreach( $data as $key => $value ){
+            $order = ( int ) ( $key / 2 );
+            if( $key % 2 ) $sql .= ", '".$value['value']."', ".$order." )";//\r\n
+            else $sql .= ($key ? ',' :'' )."( ".substr($value['name'], 4).", '".$value['value']."'";
+        }
+        $sql .= " ) UPDATE event_category SET label = d.label, color = d.color, cat_order = d.cat_order FROM new_data d WHERE d.id = event_category.id";
+        echo $GLOBALS['dbh']->query( $sql );
+    }
+
+    function deleteCategory( $delCat ){
+        $sql = "DELETE FROM event_category WHERE id = $delCat";
+        echo $GLOBALS['dbh']->query( $sql );
+    }
+
  ?>

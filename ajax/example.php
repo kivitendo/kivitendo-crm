8bdee1190c8843530cd147a7bd58c8f1e5a2bed2
Ronny Zimmermann <ronny@inter-data.de>
2023-06-28 08:58:43 +0200

    Kommentare wurden hinzugefügt.

--- ajax/example.php
+++ ajax/example.php
@@ -1,35 +1,31 @@
-  <?php
+<?php 
+    require_once __DIR__.'/../inc/ajax2function.php';
 
-  require_once __DIR__.'/../inc/ajax2function.php';
-
-
-  function newEntry( $data ){
-      writeLog( $data );
-      $data = json_decode( $data );
-      $data = ( array ) $data;
-      writeLog($data);
-      $rs = $GLOBALS[ 'dbh' ]->insert( 'example', array( 'date_time', 'c_name', 'c_age', 'c_comments' ), array( $data['datetime'], $data['name'],$data['age'], $data['comments']) );
-      echo 1;
+  
+    function newEntry( $data ){
+        writeLog( $data );
+        $data = json_decode( $data );
+        $data = ( array ) $data;
+        writeLog($data);
+          $rs = $GLOBALS[ 'dbh' ]->insert( 'example', array( 'date_time', 'c_name', 'c_age', 'c_comments' ), array( $data['datetime'], $data['name'],$data['age'], $data['comments']) );
+          echo 1;
   }
 
 
-  function getData(){
-      //alle Datensätze bereitstellen
-      $rs = $GLOBALS[ 'dbh' ]->getAll( 'SELECT * FROM example', true );//getOne
-      writeLog( 'hello' );
-      echo $rs;
+    function firstnameToGender( $data ){
+        // schreibt in die Logs einer Datei
+        writeLog( $data['name'] );
+        // Datenbank Befehl
+        $sql = "SELECT gender FROM firstnameToGender WHERE firstname ILIKE '".$data['name']."'";
+        // gibt den Befehl in der Logdatei aus
+        writeLog( 'meine Query: '.$sql );
+        // sucht in der Datenbank nach $sql
+        $rs = $GLOBALS['dbh']->getOne( $sql, true );
+        // schreibt das Ergebnis der Suche aus
+        writeLog($rs);
+        // gibt das Ergebnis der Suche zurück
+        echo $rs;
+        //'F'||'M'
+    }
 
-  }
-
-function firstnameToGender( $data ){
-    writeLog( $data['name'] );
-    $sql = " SELECT gender FROM firstnameToGender WHERE firstname ILIKE '".$data['name']."'";
-    writeLog( 'meine Query: '.$sql );
-    $rs = $GLOBALS['dbh']->getOne( $sql, true);
-      writeLog($rs);
-    echo $rs;
-     //'F'||'M'
-}
-
-
-  ?>
+?>

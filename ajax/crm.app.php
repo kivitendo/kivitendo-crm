81d89b1fd5347c371332d50bf0db4d2d6f9922bd
Dirk Schwartzer <dirk@inter-data.de>
2023-07-19 16:58:15 +0200

    crm app: Unötiger Code entfernt
    ToDo: Bug in Auftargssuche (Falsch Kontaktdaten)

--- ajax/crm.app.php
+++ ajax/crm.app.php
@@ -126,24 +126,6 @@ function searchOrder( $data ){
         $where .= " oe.status = '".$data['status']."'  AND ";
     }
 
-//    $sql = "SELECT distinct on ( init_ts, internal_order ) * FROM ( ";
-//
-//    $sql.= "SELECT distinct on ( oe.id, internal_order ) 'true' ::BOOL AS instruction, oe.id,lxc_cars.c_ln, to_char( oe.transdate, 'DD.MM.YYYY') AS transdate, ";
-//    $sql.= "oe.ordnumber, instructions.description, oe.car_status, oe.status, oe.finish_time, customer.name AS owner, oe.c_id AS c_id, oe.customer_id, ";
-//    $sql.= "lxc_cars.c_2 AS c_2, lxc_cars.c_3 AS c_3, oe.car_manuf AS car_manuf, oe.car_type AS car_type, oe.internalorder AS internal_order, oe.itime AS init_ts ";
-//    $sql.= "FROM oe, instructions, parts, lxc_cars, customer ";
-//    $sql.= "WHERE ".$where." instructions.trans_id = oe.id AND parts.id = instructions.parts_id AND lxc_cars.c_id = oe.c_id AND customer.id = oe.customer_id ";
-//
-//    $sql.= "UNION ";
-//
-//    $sql.= "SELECT distinct on ( oe.id, internal_order ) 'false'::BOOL AS instruction, oe.id,lxc_cars.c_ln, to_char( oe.transdate, 'DD.MM.YYYY') AS transdate, ";
-//    $sql.= "oe.ordnumber, orderitems.description, oe.car_status, oe.status, oe.finish_time, customer.name AS owner, oe.c_id AS c_id, oe.customer_id, ";
-//    $sql.= "lxc_cars.c_2 AS c_2, lxc_cars.c_3 AS c_3, oe.car_manuf AS car_manuf, oe.car_type AS car_type, oe.internalorder AS internal_order, oe.itime AS init_ts ";
-//    $sql.= "FROM oe, orderitems, parts, lxc_cars, customer ";
-//    $sql.= "WHERE ".$where." orderitems.trans_id = oe.id AND parts.id = orderitems.parts_id AND orderitems.position = 1 AND lxc_cars.c_id = oe.c_id AND customer.id = oe.customer_id ORDER BY instruction ASC";
-//
-//    $sql.= ") AS myTable ORDER BY internal_order ASC, init_ts DESC LIMIT 100";
-
     $sql = "SELECT distinct on ( init_ts, internal_order ) * FROM ".
             "( SELECT distinct on ( oe.id, internal_order ) 'true' ::BOOL AS instruction, oe.id, cars.c_ln, to_char( oe.transdate, 'DD.MM.YYYY') AS transdate, ".
             "oe.ordnumber, instructions.description, oe.car_status, oe.status, oe.finish_time, customer.name AS owner, oe.c_id AS c_id, oe.customer_id, ".

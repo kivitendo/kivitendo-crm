d7998ff17cacdf7b8cebd70d5eba2ee35d0eab48
Dirk Schwartzer <dirk@inter-data.de>
2023-11-01 17:11:57 +0100

    crm app: Die Dateien für den Versuch den Kalendar zu integrieren

---
+++ js/crm.app/calendar.js
@@ -0,0 +1,9 @@
+function crmCalendarView( ){
+    //window.open("crm/calendar_test.html", "_blank" );
+    crmOpenView( 'crm-plugin-calendar', null, ' - Dokumente' );
+}
+
+function crmCalendarCloseView(){
+    crmCloseView( 'crm-plugin-calendar' );
+}
+

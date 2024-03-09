7d860f159c73a4c10dfa18e0e11bd0ab126c0a96
Ronny Zimmermann <ronny@inter-data.de>
2024-03-09 14:05:03 +0100

    SQL-File für LxC-Mod hinzugefügt

---
+++ dev/installLxCarsInCRM.sql
@@ -0,0 +1,72 @@
+DROP TABLE IF EXISTS lxckba;
+
+CREATE TABLE IF NOT EXISTS lxckba (
+    hsn             TEXT NOT NULL,
+    tsn             TEXT NOT NULL,
+    hersteller      TEXT NOT NULL,
+    marke           TEXT NOT NULL,
+    name            TEXT,
+    datum           TEXT,
+    klasse          TEXT,
+    aufbau          TEXT,
+    kraftstoff      TEXT,
+    leistung        TEXT,
+    hubraum         TEXT,
+    achsen          TEXT,
+    antrieb         TEXT,
+    sitze           TEXT,
+    masse           TEXT, --End KBA
+    fhzart          TEXT, --Fahrzeugart
+    d3              TEXT, --Handelsbezeichnung
+    j               TEXT, --Fahrzeugklasse
+    field_4         TEXT, --Art des Aufbaus
+    d1              TEXT, --Marke
+    d2              TEXT, --Typ !!setzt sich aus d2_1 + d2_2 + d2_3 + d2_4 zusammen
+    field_2         TEXT, --Hersteller-Kurzbezeichnung
+    field_5         TEXT, --Bezeichnung der Fahrzeugklasse und des Aufbaus !! d_5_1 + d_5_2
+    v9              TEXT, --Schadstoffklasse für die EG-Typgenehmigung
+    field_14        TEXT, --Bezeichnung der nationalen Emissionsklasse
+    p3              TEXT, --Kraftstoffart oder Energiequelle
+    field_10        TEXT, --Kraftstoff- bzw. Energiecode
+    field_14_1      TEXT, --Code EG Schadstoffklasse
+    p1              TEXT, --Hubraum
+    l               TEXT, --Anzahl der Achsen
+    field_9         TEXT, --Anzahl der Antriebsachsen
+    p2_p4           TEXT, --maximale Leistung bei Drehzahl
+    t               TEXT, --Höchstgeschwindigkeit
+    field_18        TEXT, --Fahrzeuglänge
+    field_19        TEXT, --Fahrzeugbreite
+    field_20        TEXT, --Fahrzeughöhe
+    g               TEXT, --Leermasse
+    field_12        TEXT, --Tankvolumen
+    field_13        TEXT, --Stützlast
+    q               TEXT, --Leistungsgewicht
+    v7              TEXT, --CO2 g/km
+    f1              TEXT, --zulässige Gesamtmasse in kg
+    f2              TEXT, --zulässige Gesamtmasse in kg im Zulassungsmitgliedstaat
+    field_7_1       TEXT, --maximale Achslast Achse 1
+    field_7_2       TEXT, --maximale Achslast Achse 2
+    field_7_3       TEXT, --maximale Achslast Achse 3
+    field_8_1       TEXT, --maximale Achslast Achse 1 im Zulassungsmitgliedstaat
+    field_8_2       TEXT, --maximale Achslast Achse 2 im Zulassungsmitgliedstaat
+    field_8_3       TEXT, --maximale Achslast Achse 3 im Zulassungsmitgliedstaat
+    u1              TEXT, --Standgeräusch
+    u2              TEXT, --Drehzahl zum Standgeräusch
+    u3              TEXT, --Fahrgeräusch
+    o1              TEXT, --zulässige Anhängelast in kg gebremst
+    o2              TEXT, --zulässige Anhängelast in kg ungebremst
+    s1              TEXT, --Anzahl der Sitzplätze
+    s2              TEXT, --Anzahl der Stehplätze
+    field_15_1      TEXT, --Bereifung Achse 1
+    field_15_2      TEXT, --Bereifung Achse 2
+    field_15_3      TEXT, --Bereifung Achse 3
+    k               TEXT, --Nummer der EG Typgenehmigung
+    field_6         TEXT, --Datum der EG Typgenehmigung
+    field_17        TEXT, --Merkmal zur Betriebserlaubnis
+    field_21        TEXT, --Sonstige Vermerke,
+    id              INT NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    CONSTRAINT lxckba_pkey PRIMARY KEY( id ),
+    CONSTRAINT lxckba_unique UNIQUE( hsn, tsn, d2 )
+);
+COPY lxckba FROM '/var/www/kivitendo-crm/data/lxckba.csv' DELIMITER '|' CSV;
+
